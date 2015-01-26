53d0a15891c051c4e92d3011de5aac6ceac8ae2b
Dale Bustad <dale@divmain.com>
2015-01-26 15:10:35 -0800

    provide mechanism to determine syntax file to use for a file with any given extension

--- common/util/__init__.py
+++ common/util/__init__.py
@@ -1,5 +1,9 @@
 import sublime
+from plistlib import readPlistFromBytes
+
 from .parse_diff import parse_diff
+
+syntax_file_map = {}
 
 
 def move_cursor(view, line_no, char_no):
@@ -20,3 +24,29 @@ def move_cursor(view, line_no, char_no):
 def get_lines_from_regions(view, regions):
     full_line_regions = (view.full_line(region) for region in regions)
     return [line for region in full_line_regions for line in view.substr(region).split("\n")]
+
+
+def determine_syntax_files():
+    syntax_files = sublime.find_resources("*.tmLanguage")
+    for syntax_file in syntax_files:
+        print("trying:", syntax_file)
+        try:
+            # Use `sublime.load_resource`, in case Package is `*.sublime-package`.
+            resource = sublime.load_resource(syntax_file)
+            plist = readPlistFromBytes(bytearray(resource, encoding="utf-8"))
+            for extension in plist["fileTypes"]:
+                if extension not in syntax_file_map:
+                    syntax_file_map[extension] = []
+                extension_list = syntax_file_map[extension]
+                extension_list.append(syntax_file)
+        except:
+            continue
+
+
+def get_syntax_for_file(filename):
+    extension = filename.split(".")[-1]
+    try:
+        # Return last syntax file applicable to this extension.
+        return syntax_file_map[extension][-1]
+    except:
+        return None
--- git_gadget.py
+++ git_gadget.py
@@ -14,4 +14,8 @@ log.set_level(0)
 if sys.version_info[0] == 2:
     raise ImportWarning("GitGadget does not support Sublime Text 2.")
 else:
+    def plugin_loaded():
+        from .common import util
+        util.determine_syntax_files()
+
     from .commands import *
