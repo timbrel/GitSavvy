5ba2e177c6db6f2d85e20f50bc6a58481cada8b2
Dale Bustad <dale@divmain.com>
2015-01-26 15:09:46 -0800

    better submodule reloading for debugging purposes

--- git_gadget.py
+++ git_gadget.py
@@ -2,10 +2,12 @@ import sys
 import imp
 from .common import log
 
-for name, module in sys.modules.items():
-    if name[0:9] == "GitGadget":
-        print("reloading " + name)
-        imp.reload(module)
+# Reload all submodules when debugging.
+for _ in range(2):
+    for name, module in sys.modules.items():
+        if name[0:9] == "GitGadget":
+            print("reloading " + name)
+            imp.reload(module)
 
 log.set_level(0)
 
