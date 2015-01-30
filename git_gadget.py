3c4ceb9c73e1ae5015b4288ad4bb1cb36aec8e10
Dale Bustad <dale@divmain.com>
2015-01-30 00:00:17 -0800

    only reload submodules if dev_mode is specified in GitGadget settings

--- git_gadget.py
+++ git_gadget.py
@@ -1,13 +1,7 @@
 import sys
+import sublime
 import imp
 from .common import log
-
-# Reload all submodules when debugging.
-for _ in range(2):
-    for name, module in sys.modules.items():
-        if name[0:9] == "GitGadget":
-            print("reloading " + name)
-            imp.reload(module)
 
 log.set_level(0)
 
@@ -15,6 +9,16 @@ if sys.version_info[0] == 2:
     raise ImportWarning("GitGadget does not support Sublime Text 2.")
 else:
     def plugin_loaded():
+        gadget_settings = sublime.load_settings("GitGadget.sublime-settings")
+
+        if gadget_settings.get("dev_mode"):
+            # Reload all submodules when debugging.
+            for _ in range(2):
+                for name, module in sys.modules.items():
+                    if name[0:9] == "GitGadget":
+                        print("reloading " + name)
+                        imp.reload(module)
+
         from .common import util
         util.determine_syntax_files()
 
