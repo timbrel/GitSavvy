b151d703b6b10d7fd51d4c80fceac2c90609dab6
Dale Bustad <dale@divmain.com>
2015-02-06 01:46:43 -0800

    disambiguate payloads in interwebs common module

--- common/interwebs.py
+++ common/interwebs.py
@@ -22,18 +22,18 @@ def request(verb, host, port, path, payload=None, timeout=10, https=False, heade
     connection.request(verb, path, body=payload, headers=headers)
 
     response = connection.getresponse()
-    payload = response.read()
+    response_payload = response.read()
     headers = dict(response.getheaders())
     status = response.status
 
     is_json = "application/json" in headers["Content-Type"]
     if is_json:
-        payload = json.loads(payload.decode("utf-8"))
+        response_payload = json.loads(response_payload.decode("utf-8"))
 
     response.close()
     connection.close()
 
-    return Response(payload, headers, status, is_json)
+    return Response(response_payload, headers, status, is_json)
 
 
 get = partial(request, "GET")
