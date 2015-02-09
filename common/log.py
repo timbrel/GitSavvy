332dfb35de36c5160d2848c483ff96b61b69165b
Dale Bustad <dale@divmain.com>
2015-02-09 11:49:32 -0800

    when logging errors, cast any msgs to str first

--- common/log.py
+++ common/log.py
@@ -10,7 +10,7 @@ PANEL_NAME = "GitSavvy"
 
 
 def panel(*msgs):
-    msg = "\n".join(msgs)
+    msg = "\n".join(str(msg) for msg in msgs)
     sublime.active_window().active_view().run_command("gs_display_panel", {"msg": msg})
 
 
