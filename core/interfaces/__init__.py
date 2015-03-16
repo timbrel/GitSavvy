2b3b326a747afbf45696c1e637755009228dc242
Dale Bustad <dale@divmain.com>
2015-03-16 14:03:04 -0700

    Progress: Branch dashboard is rendering and re-rendering when regaining focus.

--- core/interfaces/__init__.py
+++ core/interfaces/__init__.py
@@ -1,1 +1,2 @@
 from .status import *
+from .branch import *
---
+++ core/interfaces/branch.py
@@ -0,0 +1,143 @@
+import os
+from itertools import groupby
+
+from ...common import ui
+from ..git_command import GitCommand
+
+from sublime_plugin import WindowCommand
+
+
+interfaces = {}
+
+
+class GsShowBranchCommand(WindowCommand, GitCommand):
+
+    """
+    Open a branch dashboard for the active Git repository.
+    """
+
+    def run(self):
+        BranchInterface(view_attrs={"git_savvy.repo_path": self.repo_path})
+
+
+class BranchInterface(ui.Interface, GitCommand):
+
+    """
+    """
+
+    interface_type = "branch"
+    read_only = True
+    view_type = "branches"
+    syntax_file = "Packages/GitSavvy/syntax/branch.tmLanguage"
+    word_wrap = False
+
+    dedent = 4
+    skip_first_line = True
+
+    template = """
+
+      BRANCH:  {branch_status}
+      ROOT:    {git_root}
+      HEAD:    {head}
+
+      LOCAL:
+    {branch_list}{remotes}
+      ###########
+      ## LOCAL ##
+      ###########
+
+      [n] create new branch
+      [N] create new branch from selected
+      [d] delete
+      [r] rename
+
+      [c] checkout
+
+      [t] track remote
+
+      [p] push selected to remote
+      [P] push all branches to remote
+
+      [m] merge selected into active branch
+
+      [f] diff against active
+
+      [l] fetch and merge into active branch
+
+
+      ###########
+      ## OTHER ##
+      ###########
+
+      [r] refresh
+
+    -
+    """
+
+    template_remote = """
+      REMOTE ({remote_name}):
+    {remote_branch_list}"""
+
+    def title(self):
+        return "BRANCHES: {}".format(os.path.basename(self.repo_path))
+
+    def pre_render(self):
+        self._branches = tuple(self.get_branches())
+
+    @ui.partial("branch_status")
+    def render_branch_status(self):
+        return self.get_branch_status()
+
+    @ui.partial("git_root")
+    def render_git_root(self):
+        return self.repo_path
+
+    @ui.partial("head")
+    def render_head(self):
+        return self.get_latest_commit_msg_for_head()
+
+    @ui.partial("branch_list")
+    def render_branch_list(self, branches=None):
+        if not branches:
+            branches = [branch for branch in self._branches if not branch.remote]
+
+        return "\n".join(
+            "  {indicator} {hash:.7} {name} {tracking}".format(
+                indicator="▸" if branch.active else " ",
+                hash=branch.commit_hash,
+                name=branch.name,
+                tracking=("({branch}{status})".format(
+                    branch=branch.tracking,
+                    status=", " + branch.tracking_status if branch.tracking_status else ""
+                    )
+                    if branch.tracking else "")
+                )
+            for branch in branches
+            )
+
+    @ui.partial("remotes")
+    def render_remotes(self):
+        output_tmpl = "\n"
+        render_fns = []
+
+        for remote_name, branches in groupby(self._branches, lambda branch: branch.remote):
+            if not remote_name:
+                continue
+
+            branches = tuple(branches)
+            key = "branch_list_" + remote_name
+            output_tmpl += "{" + key + "}\n"
+
+            @ui.partial(key)
+            def render(remote_name=remote_name, branches=branches):
+                return self.template_remote.format(
+                    remote_name=remote_name,
+                    remote_branch_list=self.render_branch_list(branches=branches)
+                    )
+
+            render_fns.append(render)
+
+        return output_tmpl, render_fns
+
+
+ui.register_listeners(BranchInterface)
