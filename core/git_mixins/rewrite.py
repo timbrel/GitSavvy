9a56cf6cff9dd968b49d264123375e8448de594a
Dale Bustad <dale@divmain.com>
2015-04-05 22:10:02 -0800

    Add helper methods to RewriteMixin for rebase dashboard.

--- core/git_mixins/rewrite.py
+++ core/git_mixins/rewrite.py
@@ -1,3 +1,4 @@
+import os
 from types import SimpleNamespace
 
 
@@ -56,3 +57,36 @@ class RewriteMixin():
 
             self.git("reset", "--hard")
             self.git("checkout", branch_name)
+
+    @property
+    def _rebase_dir(self):
+        return os.path.join(self.repo_path, ".git", "rebase-apply")
+
+    def in_rebase(self):
+        return os.path.isdir(self._rebase_dir)
+
+    def rebase_orig_head(self):
+        path = os.path.join(self._rebase_dir, "orig-head")
+        with open(path, "r") as f:
+            return f.read().strip()
+
+    def rebase_conflict_at(self):
+        path = os.path.join(self._rebase_dir, "original-commit")
+        with open(path, "r") as f:
+            return f.read().strip()
+
+    def rebase_branch_name(self):
+        path = os.path.join(self._rebase_dir, "head-name")
+        with open(path, "r") as f:
+            return f.read().strip().replace("refs/heads/", "")
+
+    def rebase_onto_commit(self):
+        path = os.path.join(self._rebase_dir, "onto")
+        with open(path, "r") as f:
+            return f.read().strip()
+
+    def rebase_rewritten(self):
+        path = os.path.join(self._rebase_dir, "rewritten")
+        with open(path, "r") as f:
+            entries = f.read().strip().split("\n")
+            return (entry.split(" ") for entry in entries)
