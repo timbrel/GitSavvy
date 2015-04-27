295bb651f269702eb05b343ae7286e2d1f512b4e
Dale Bustad <dale@divmain.com>
2015-04-26 18:41:41 -0700

    Bug: Native line-ending used on Windows when autocrlf is false.
    Closes #150.

--- core/git_mixins/ignore.py
+++ core/git_mixins/ignore.py
@@ -1,11 +1,24 @@
 import os
 
 
+linesep = None
+
+
 class IgnoreMixin():
 
     def add_ignore(self, path_or_pattern):
         """
         Add the provided relative path or pattern to the repo's `.gitignore` file.
         """
+        global linesep
+
+        if not linesep:
+            # Use native line ending on Windows only when `autocrlf` is set to `true`.
+            if os.name == "nt":
+                autocrlf = self.git("config", "--global", "core.autocrlf").strip() == "true"
+                linesep = os.linesep if autocrlf else "\n"
+            else:
+                linesep = os.linesep
+
         with open(os.path.join(self.repo_path, ".gitignore"), "at") as ignore_file:
-            ignore_file.write(os.linesep + "# added by GitSavvy" + os.linesep + path_or_pattern + os.linesep)
+            ignore_file.write(linesep + "# added by GitSavvy" + linesep + path_or_pattern + linesep)
