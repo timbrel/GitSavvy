17dd720a8a8681196ad8a14a917ebb1768f3404a
Petr Marek <contact@petrmarek.eu>
2016-04-01 22:37:49 +0200

    Fix log_graph keybinds

--- core/commands/log_graph.py
+++ core/commands/log_graph.py
@@ -23,6 +23,18 @@ class GsLogGraphCommand(WindowCommand, GitCommand):
         view.settings().set("git_savvy.tabbable", True)
         view.settings().set("word_wrap", False)
         view.settings().set("all_branches", all_branches)
+        sublime_settings = sublime.load_settings("GitSavvy.sublime-settings")
+        if sublime_settings.get("vintageous_friendly", False) is True:
+            view.settings().set("git_savvy.handle_keys", False)
+            view.settings().set("git_savvy.vintageous_friendly", True)
+            should_enter_insert_mode = sublime_settings.get("vintageous_enter_insert_mode", False)
+            view.settings().set("git_savvy.should_enter_insert_mode", should_enter_insert_mode)
+            if should_enter_insert_mode is True:
+                view.run_command("gs_vintageous_enter_insert_mode")
+        else:
+            view.settings().set("git_savvy.handle_keys", True)
+            view.settings().set("git_savvy.vintageous_friendly", False)
+            view.settings().set("git_savvy.should_enter_insert_mode", False)
         view.set_name(LOG_GRAPH_TITLE)
         view.set_scratch(True)
         view.set_read_only(True)
