31fef9837d2ede9b79592088236385849d4047de
Petr Marek <contact@petrmarek.eu>
2016-05-28 23:52:04 +0200

    handle_semver_tags wip

--- core/git_mixins/tags.py
+++ core/git_mixins/tags.py
@@ -1,4 +1,6 @@
+import re
 from collections import namedtuple
+from distutils.version import LooseVersion
 
 
 TagDetails = namedtuple("TagDetails", ("sha", "tag"))
@@ -23,6 +25,7 @@ class TagsMixin():
             porcelain_entries.reverse()
 
         entries = [TagDetails(entry[:40], entry[51:]) for entry in iter(porcelain_entries) if entry]
+        entries = self.handle_semver_tags(entries)
 
         return entries
 
@@ -33,3 +36,15 @@ class TagsMixin():
 
         tag = self.git("describe", "--tags", "--abbrev=0", throw_on_stderr=False).strip()
         return tag
+
+    def handle_semver_tags(self, entries):
+        """
+        Sorts tags using LooseVersion if there's a tag matching the semver format.
+        """
+
+        semver_test = re.compile('\d+\.\d+\.\d+')
+        semver_entries = [True for entry in entries if semver_test.match(entry.tag)]
+        semver_tags_present = len(semver_entries) > 0
+        if semver_tags_present:
+            entries = sorted(entries, key=lambda entry: LooseVersion(entry.tag), reverse=True)
+        return entries
