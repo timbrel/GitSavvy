174ef25e0714b5187b4713091be2ea9bfd9d3384
Simon <simon.toivo@telhaug.no>
2016-06-15 23:53:49 +0200

    Improve: semver tags sorting when failure

--- core/git_mixins/tags.py
+++ core/git_mixins/tags.py
@@ -42,14 +42,24 @@ class TagsMixin():
         Sorts tags using LooseVersion if there's a tag matching the semver format.
         """
 
-        semver_test = re.compile('v?\d+\.\d+\.?\d*')
+        semver_test = re.compile(r'\d+\.\d+\.?\d*')
 
         semver_entries, regular_entries = [], []
         for entry in entries:
-            if semver_test.match(entry.tag):
+            if semver_test.search(entry.tag):
                 semver_entries.append(entry)
             else:
                 regular_entries.append(entry)
         if len(semver_entries):
-            semver_entries = sorted(semver_entries, key=lambda entry: LooseVersion(entry.tag), reverse=True)
+            try:
+                semver_entries = sorted(semver_entries, key=lambda entry: LooseVersion(entry.tag), reverse=True)
+            except Exception as e:
+                # the error might me caused of having tags like 1.2.3.1, 1.2.3.beta
+                # exception is cant convert str to int, it is comparing 'beta' to 1
+                # if that fails then only take the numbers and sort them
+                semver_entries = sorted(
+                    semver_entries,
+                    key=lambda entry: LooseVersion(semver_test.search(entry.tag).group()),
+                    reverse=True)
+
         return semver_entries + regular_entries
