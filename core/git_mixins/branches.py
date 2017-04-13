a4b2a469ee155629a7241704754445be9ad5fd56
Vlad Tsepelev <reg.yeti@gmail.com>
2017-04-13 19:57:53 +0300

    Sort branches by passed sort param

--- core/git_mixins/branches.py
+++ core/git_mixins/branches.py
@@ -18,12 +18,10 @@ Branch = namedtuple("Branch", (
 
 class BranchesMixin():
 
-    def get_branches(self):
+    def get_branches(self, sort_by_recent=None):
         """
         Return a list of all local and remote branches.
         """
-        savvy_settings = sublime.load_settings("GitSavvy.sublime-settings")
-        sort_by_recent = savvy_settings.get("sort_by_recent_in_branch_dashboard")
         stdout = self.git("branch", "-a", "-vv", "--no-abbrev", "--no-color", "--sort=-committerdate" if sort_by_recent else None)
         return (branch
                 for branch in (self._parse_branch_line(self, line) for line in stdout.split("\n"))
