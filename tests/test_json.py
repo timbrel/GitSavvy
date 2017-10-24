7e712f13c5e1abd878ea063043eba7300a13e449
Simon <simon.toivo@telhaug.no>
2017-10-24 21:40:22 +0200

    Internal: Fix test_json.py from @facelessuser
    https://github.com/divmain/GitSavvy/commit/4bc4bf32636df4a5f620193bbffe34686ec76bf9

--- tests/test_json.py
+++ tests/test_json.py
@@ -27,7 +27,6 @@ RE_COMMENT = re.compile(
         )
       | (?P<code>
             "(?:\\.|[^"\\])*"               # double quotes
-          | '(?:\\.|[^'\\])*'               # single quotes
           | .[^/"']*                        # everything else
         )
     ''',
@@ -49,7 +48,6 @@ RE_TRAILING_COMMA = re.compile(
         )
       | (?P<code>
             "(?:\\.|[^"\\])*"            # double quoted string
-          | '(?:\\.|[^'\\])*'            # single quoted string
           | .[^,"']*                     # everything else
         )
     ''',
@@ -186,8 +184,7 @@ class CheckJsonFormat(object):
             count = 1
             for line in f:
                 indent_match = RE_LINE_INDENT[self.use_tabs].match(line)
-                end_comment = (comment_align is not None and
-                               RE_COMMENT_END.search(line))
+                end_comment = (comment_align is not None or indent_match.group(2)) and RE_COMMENT_END.search(line)
                 # Don't allow empty lines at file start.
                 if count == 1 and line.strip() == '':
                     self.log_failure(W_NL_START, count)
