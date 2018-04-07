daaf37d6c9ee381477a102a491a68bc2d25615a3
Randy Lai <randy.cs.lai@gmail.com>
2018-04-06 23:29:45 -0400

    Feature: git: checkout current file at commit

--- core/commands/checkout.py
+++ core/commands/checkout.py
@@ -2,6 +2,7 @@ import sublime
 from sublime_plugin import WindowCommand
 
 from ..git_command import GitCommand
+from .log import LogMixin
 from ...common import util
 from ..ui_mixins.quick_panel import show_branch_panel
 
@@ -35,7 +36,7 @@ class GsCheckoutBranchCommand(WindowCommand, GitCommand):
 
         self.git("checkout", branch)
         self.window.status_message("Checked out `{}` branch.".format(branch))
-        util.view.refresh_gitsavvy_interfaces(self.window, refresh_sidebar=True)
+        util.view.refresh_gitsavvy(self.window.active_view(), refresh_sidebar=True)
 
 
 class GsCheckoutNewBranchCommand(WindowCommand, GitCommand):
@@ -65,9 +66,10 @@ class GsCheckoutNewBranchCommand(WindowCommand, GitCommand):
             branch_name,
             self.base_branch if self.base_branch else None)
         self.window.status_message("Created and checked out `{}` branch.".format(branch_name))
-        util.view.refresh_gitsavvy_interfaces(self.window,
-                                              refresh_sidebar=True,
-                                              interface_reset_cursor=True)
+        util.view.refresh_gitsavvy(
+            self.window.active_view(),
+            refresh_sidebar=True,
+            interface_reset_cursor=True)
 
 
 class GsCheckoutRemoteBranchCommand(WindowCommand, GitCommand):
@@ -114,9 +116,11 @@ class GsCheckoutRemoteBranchCommand(WindowCommand, GitCommand):
         self.git("checkout", "-b", branch_name, "--track", self.remote_branch)
         self.window.status_message(
             "Checked out `{}` as local branch `{}`.".format(self.remote_branch, branch_name))
-        util.view.refresh_gitsavvy_interfaces(self.window,
-                                              refresh_sidebar=True,
-                                              interface_reset_cursor=True)
+        util.view.refresh_gitsavvy(
+            self.window.active_view(),
+            refresh_sidebar=True,
+            interface_reset_cursor=True
+        )
 
 
 class GsCheckoutCurrentFileCommand(WindowCommand, GitCommand):
@@ -129,3 +133,21 @@ class GsCheckoutCurrentFileCommand(WindowCommand, GitCommand):
         if self.file_path:
             self.checkout_file(self.file_path)
             self.window.status_message("Successfully checked out {} from head.".format(self.file_path))
+            util.view.refresh_gitsavvy_interfaces(self.window, interface_reset_cursor=True)
+
+
+class GsCheckoutCurrentFileAtCommitCommand(LogMixin, WindowCommand, GitCommand):
+
+    """
+    Reset the current active file to a given commit.
+    """
+
+    def run(self):
+        if self.file_path:
+            super().run(file_path=self.file_path)
+
+    def do_action(self, commit_hash, **kwargs):
+        if commit_hash:
+            self.checkout_ref(commit_hash, self.file_path)
+            self.window.status_message("Successfully checked out {} from head.".format(self.file_path))
+            util.view.refresh_gitsavvy_interfaces(self.window, interface_reset_cursor=True)
