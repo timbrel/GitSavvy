e41823e7e11e3813bb4dfd188f4f024d1f43d23a
herr kaste <herr.kaste@gmail.com>
2018-11-14 11:07:42 +0100

    Add tests for long branch status text
    This text is used in the dashboards

--- tests/test_repo_status.py
+++ tests/test_repo_status.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 import sublime
 
 from unittesting import DeferrableTestCase
@@ -9,17 +11,6 @@ from GitSavvy.core.git_command import GitCommand
 class TestPrivateGitStatusFunction(DeferrableTestCase):
     def tearDown(self):
         unstub()
-    # def setUp(self):
-    #     self.view = sublime.active_window().new_file()
-    #     # make sure we have a window to work with
-    #     s = sublime.load_settings("Preferences.sublime-settings")
-    #     s.set("close_windows_when_empty", False)
-
-    # def tearDown(self):
-    #     if self.view:
-    #         self.view.set_scratch(True)
-    #         self.view.window().focus_view(self.view)
-    #         self.view.window().run_command("close_file")
 
     def test_ensure_it_calls_git_status(self):
         git = GitCommand()
@@ -130,30 +121,151 @@ class TestShortBranchStatus(DeferrableTestCase):
 
     # TODO: Add tests for `in_rebase` True
     # TODO: Add tests for `in_merge` True
-    # TODO: Add tests for
+    # TODO: Add tests for ?
 
 
 TestLongBranchStatusTestcases = [
-("""\
-## optimize-status-interface...fork/optimize-status-interface [ahead 1]
-?? tests/test_repo_status.py""".strip().splitlines(), "optimize-status-interface*+1"),
-
-("""\
-## dev...origin/dev [behind 7]
-?? tests/test_repo_status.py
-""".strip().splitlines(), "dev*-7"),
-
-("""\
-## optimize-status-interface...fork/optimize-status-interface [ahead 1, behind 2]
-?? tests/test_repo_status.py
-""".strip().splitlines(), "optimize-status-interface*+1-2"),
-
-("""\
-## improve-diff-view...fork/improve-diff-view [gone]
-?? tests/test_repo_status.py
-""".strip().splitlines(), "improve-diff-view*")
-
+(
+    "## optimize-status-interface",
+    "On branch `optimize-status-interface`."
+),
+(
+    "## optimize-status-interface\x00",
+    "On branch `optimize-status-interface`."
+),
+(
+    "## optimize-status-interface\x00?? foo.txt",
+    "On branch `optimize-status-interface`."
+),
+(
+    "## optimize-status-interface...fork/branch-name",
+    "On branch `optimize-status-interface` tracking `fork/branch-name`."
+),
+(
+    "## optimize-status-interface...fork/branch-name [ahead 1]",
+    dedent("""\
+    On branch `optimize-status-interface` tracking `fork/branch-name`.
+    You're ahead by 1.
+    """.rstrip())
+),
+(
+    "## optimize-status-interface...fork/branch-name [ahead 1]\x00?? foo.txt",
+    dedent("""\
+    On branch `optimize-status-interface` tracking `fork/branch-name`.
+    You're ahead by 1.
+    """.rstrip())
+),
+(
+    "## dev...origin/dev [behind 7]",
+    dedent("""\
+    On branch `dev` tracking `origin/dev`.
+    You're behind by 7.
+    """.rstrip())
+),
+(
+    "## dev...origin/dev [behind 7]\x00?? foo.txt",
+    dedent("""\
+    On branch `dev` tracking `origin/dev`.
+    You're behind by 7.
+    """.rstrip())
+),
+(
+    "## optimize-status-interface...fork/branch-name [ahead 1, behind 2]",
+    dedent("""\
+    On branch `optimize-status-interface` tracking `fork/branch-name`.
+    You're ahead by 1 and behind by 2.
+    """.rstrip())
+),
+(
+    "## optimize-status-interface...fork/branch-name [ahead 1, behind 2]\x00M foo",
+    dedent("""\
+    On branch `optimize-status-interface` tracking `fork/branch-name`.
+    You're ahead by 1 and behind by 2.
+    """.rstrip())
+),
+(
+    "## improve-diff-view...fork/improve-diff-view [gone]",
+    dedent("""\
+    On branch `improve-diff-view` tracking `fork/improve-diff-view`.
+    The remote branch is gone.
+    """.rstrip())
+),
+(
+    "## improve-diff-view...fork/improve-diff-view [gone]\x00?? foo",
+    dedent("""\
+    On branch `improve-diff-view` tracking `fork/improve-diff-view`.
+    The remote branch is gone.
+    """.rstrip())
+),
+(
+    "## HEAD (no branch)",
+    "HEAD is in a detached state."
+),
+(
+    "## HEAD (no branch)\x00?? foo",
+    "HEAD is in a detached state."
+),
+(
+    "## No commits yet on master",
+    "Initial commit on `master`."
+),
+(
+    "## No commits yet on master\x00?? foo",
+    "Initial commit on `master`."
+),
+# Previous versions of git instead emitted this before the initial commit
+(
+    "## Initial commit on zoom",
+    "Initial commit on `zoom`."
+),
+(
+    "## Initial commit on master\x00?? foo",
+    "Initial commit on `master`."
+),
 ]
+
+
+class TestLongBranchStatus(DeferrableTestCase):
+    def tearDown(self):
+        unstub()
+
+    @p.expand(TestLongBranchStatusTestcases)
+    def test_format_branch_status_for_status_dashboard(self, status_lines, expected):
+        git = GitCommand()
+        when(git).in_rebase().thenReturn(False)
+        when(git).in_merge().thenReturn(False)
+
+        when(git).git("status", ...).thenReturn(status_lines)
+
+        actual = git.get_branch_status(delim="\n")
+        self.assertEqual(actual, expected)
+
+    # TODO: Add tests for `in_rebase` True
+    # TODO: Add tests for `in_merge` True
+    # TODO: Add tests for ?
+
+
+# TestLongBranchStatusTestcases = [
+# ("""\
+# ## optimize-status-interface...fork/optimize-status-interface [ahead 1]
+# ?? tests/test_repo_status.py""".strip().splitlines(), "optimize-status-interface*+1"),
+
+# ("""\
+# ## dev...origin/dev [behind 7]
+# ?? tests/test_repo_status.py
+# """.strip().splitlines(), "dev*-7"),
+
+# ("""\
+# ## optimize-status-interface...fork/optimize-status-interface [ahead 1, behind 2]
+# ?? tests/test_repo_status.py
+# """.strip().splitlines(), "optimize-status-interface*+1-2"),
+
+# ("""\
+# ## improve-diff-view...fork/improve-diff-view [gone]
+# ?? tests/test_repo_status.py
+# """.strip().splitlines(), "improve-diff-view*")
+
+# ]
 
 
 """\
