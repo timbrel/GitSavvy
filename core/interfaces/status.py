46ea00ef35f0985f3ed8078545ae77636bdd8428
herr kaste <herr.kaste@gmail.com>
2018-11-26 20:37:10 +0100

    Put the monster regex on top as a constant

--- core/interfaces/status.py
+++ core/interfaces/status.py
@@ -11,6 +11,31 @@ from ..git_command import GitCommand
 from ...common import util
 
 
+# Expected
+#  - common/commands/view_manipulation.py
+#    common/ui.py
+#    core/commands/commit_compare.py -> core/commands/commit_compare_foo.py
+# But do not match our stashes or anything from our help
+#    (1) log git start/stop
+#           [t] create stash
+EXTRACT_FILENAME_RE = (
+    r"^(?:    .+ -> |  [ -] (?!\(\d+\) ))"
+    #     ^ leading 4 spaces
+    #         ^ a filename
+    #            ^ marker indicating a rename/move
+    #               ^ OR
+    #                ^ leading 4 spaces or two spaces and our deleted marker
+    #                       ^^^^^^^^^^^ but be aware to *not* match stashes
+    r"(?!Your working directory is clean\.)"
+    #   ^ be aware to *not* match this message which otherwise fulfills our
+    #     filename matcher
+    r"(\S.*)$"
+    # ^^^^^^ the actual filename matcher
+    # Note: A filename cannot start with a space (which is luckily true anyway)
+    # otherwise our naive `.*` could consume only whitespace.
+)
+
+
 def distinct_until_state_changed(just_render_fn):
     """Custom `lru_cache`-look-alike to minimize redraws."""
     previous_state = {}
@@ -256,24 +281,7 @@ class StatusInterface(ui.Interface, GitCommand):
         self.update_state(self.fetch_repo_status, self.just_render)
 
     def after_view_creation(self, view):
-        # Expected
-        #  - common/commands/view_manipulation.py
-        #    common/ui.py
-        #    core/commands/commit_compare.py -> core/commands/commit_compare_foo.py
-        # But do not match our stashes or anything from our help
-        #    (1) log git start/stop
-        #           [t] create stash
-        file_regex = r"^(?:    .+ -> |  [ -] (?!\(\d+\) ))(?!Your working directory is clean\.)(\S.*)$"
-        #                  ^ leading 4 spaces
-        #                      ^ a filename
-        #                         ^ marker indicating a rename/move
-        #                            ^ OR
-        #                             ^ leading 4 spaces or two spaces and our deleted marker
-        #                                    ^^^^^^^^^^^ but be aware to not match stashes
-        #                                                ^^^^^^ the actual filename matcher
-        # Note: A filename cannot start with a space bc otherwise our naive `.*` could consume
-        # only whitespace.
-        view.settings().set("result_file_regex", file_regex)
+        view.settings().set("result_file_regex", EXTRACT_FILENAME_RE)
         view.settings().set("result_base_dir", self.repo_path)
 
     def on_new_dashboard(self):
