05d0270c48a699233fc48dfcf593dc8f60cc0412
herr kaste <herr.kaste@gmail.com>
2019-08-02 23:12:20 +0200

    Modernize graph view tests

--- tests/test_graph_view.py
+++ tests/test_graph_view.py
@@ -1,11 +1,8 @@
-from functools import wraps
 import os
-import sys
-from unittest.case import _ExpectedFailure, _UnexpectedSuccess
 
 import sublime
 
-from unittesting import DeferrableTestCase
+from unittesting import DeferrableTestCase, expectedFailure
 from GitSavvy.tests.parameterized import parameterized as p
 from GitSavvy.tests.mockito import unstub, when
 
@@ -17,23 +14,6 @@ from GitSavvy.core.commands.log_graph import (
 )
 from GitSavvy.core.commands.show_commit_info import GsShowCommitInfoCommand
 from GitSavvy.core.settings import GitSavvySettings
-
-
-def isiterable(obj):
-    return hasattr(obj, '__iter__')
-
-
-def expectedFailure(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            deferred = func(*args, **kwargs)
-            if isiterable(deferred):
-                yield from deferred
-        except Exception:
-            raise _ExpectedFailure(sys.exc_info())
-        raise _UnexpectedSuccess
-    return wrapper
 
 
 RUNNING_ON_LINUX_TRAVIS = os.environ.get('TRAVIS_OS_NAME') == 'linux'
@@ -136,22 +116,7 @@ class TestDiffViewInteractionWithCommitInfoPanel(DeferrableTestCase):
         self.create_new_view(window)
 
     def tearDown(self):
-        self.do_cleanup()
         unstub()
-
-    # `addCleanup` doesn't work either in Sublime at all or
-    # with the DeferrableTestCase so we do a quick implementation
-    # here.
-    def add_cleanup(self, fn, *args, **kwargs):
-        self._cleanups.append((fn, args, kwargs))
-
-    def do_cleanup(self):
-        while self._cleanups:
-            fn, args, kwrags = self._cleanups.pop()
-            try:
-                fn(*args, **kwrags)
-            except Exception:
-                pass
 
     def await_string_in_view(self, view, needle):
         yield lambda: view.find(needle, 0, sublime.LITERAL)
@@ -167,7 +132,7 @@ class TestDiffViewInteractionWithCommitInfoPanel(DeferrableTestCase):
 
     def create_new_view(self, window=None):
         view = (window or sublime.active_window()).new_file()
-        self.add_cleanup(self.close_view, view)
+        self.addCleanup(self.close_view, view)
         return view
 
     def close_view(self, view):
@@ -179,7 +144,7 @@ class TestDiffViewInteractionWithCommitInfoPanel(DeferrableTestCase):
         settings = GitSavvySettings()
         original_value = settings.get(key)
         settings.set(key, value)
-        self.add_cleanup(settings.set, key, original_value)
+        self.addCleanup(settings.set, key, original_value)
 
     def register_commit_info(self, info):
         for sha1, info in info.items():
