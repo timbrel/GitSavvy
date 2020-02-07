850b4585226f3b5044eda01b2185cf5dd948f160
herr kaste <herr.kaste@gmail.com>
2020-02-07 17:15:30 +0100

    Implement a variable `replace_region` function

--- core/commands/log_graph.py
+++ core/commands/log_graph.py
@@ -1,4 +1,4 @@
-from contextlib import contextmanager
+from contextlib import contextmanager, ExitStack
 from functools import lru_cache, partial
 from itertools import chain, islice, takewhile
 import os
@@ -186,8 +186,19 @@ DATE_FORMAT_STATE = 'trying'
 
 
 @text_command
-def replace_region(view, edit, text, region):
-    view.replace(edit, region, text)
+def replace_region(view, edit, text, region=None, wrappers=[]):
+    if region is None:
+        # If you "replace" (or expand) directly at the cursor,
+        # the cursor expands into a selection.
+        # This is a common case for an empty view so we take
+        # care of it out of box.
+        region = sublime.Region(0, max(1, view.size()))
+
+    with ExitStack() as stack:
+        for wrapper in wrappers:
+            stack.enter_context(wrapper(view))
+        stack.enter_context(writable_view(view))
+        view.replace(edit, region, text)
 
 
 @contextmanager
@@ -198,6 +209,36 @@ def writable_view(view):
         yield
     finally:
         view.set_read_only(is_read_only)
+
+
+@contextmanager
+def restore_cursors(view):
+    save_cursors = [
+        (view.rowcol(s.begin()), view.rowcol(s.end()))
+        for s in view.sel()
+    ] or [((0, 0), (0, 0))]
+
+    try:
+        yield
+    finally:
+        view.sel().clear()
+        for (begin, end) in save_cursors:
+            view.sel().add(
+                sublime.Region(view.text_point(*begin), view.text_point(*end))
+            )
+
+
+@contextmanager
+def stable_viewport(view):
+    # Ref: https://github.com/SublimeTextIssues/Core/issues/2560
+    # See https://github.com/jonlabelle/SublimeJsPrettier/pull/171/files
+    # for workaround.
+    vx, vy = view.viewport_position()
+    try:
+        yield
+    finally:
+        view.set_viewport_position((0, 0))  # intentional!
+        view.set_viewport_position((vx, vy))
 
 
 def slice_new_content(old_content, new_content):
