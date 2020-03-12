80324a5e2c61959aa781c063ece0464bd7ce80d1
herr kaste <herr.kaste@gmail.com>
2020-03-12 21:29:02 +0100

    Fix: nearest branch throwing if there is only one branch
    Fixes #1261
    
    If the repo has only one branch git changes the output format of
    `git show-branch` completely and doesn't show a prelude anymore.
    Also no column indicator. Example output:
    
    ```
    $ git show-branch
    [master] 0.22.2
    ```
    
    We branch that off by handling the `ValueError` because it is an
    edge-case.

--- core/git_mixins/rebase.py
+++ core/git_mixins/rebase.py
@@ -18,24 +18,34 @@ class NearestBranchMixin(object):
     def branch_relatives(self, branch):
         # type: (str) -> List[str]
         """ Get list of all relatives from ``git show-branch`` results """
-        output = self.git("show-branch", "--no-color")
+        output = self.git("show-branch", "--no-color")  # type: str
 
-        prelude, body = re.split(r'^-+$', output, flags=re.M)
+        try:
+            prelude, body = re.split(r'^-+$', output, flags=re.M)
+        except ValueError:
+            # If there is only one branch, git changes the output format
+            # and omits the prelude and column indicator.
+            lines = filter(None, output.splitlines())
+        else:
+            match = re.search(r'^(\s+)\*', prelude, re.M)
+            if not match:
+                print("branch {} not found in header information".format(branch))
+                return []
 
-        match = re.search(r'^(\s+)\*', prelude, re.M)
-        if not match:
-            print("branch {} not found in header information".format(branch))
-            return []
+            branch_column = len(match.group(1))
+            lines = (
+                line
+                for line in filter(None, body.splitlines())
+                if line[branch_column] != ' '
+            )
 
-        branch_column = len(match.group(1))
         relatives = []  # type: List[str]
-        for line in filter(None, body.splitlines()):  # type: str
-            if line[branch_column] != ' ':
-                match = EXTRACT_BRANCH_NAME.match(line)
-                if match:
-                    branch_name = match.group(1)
-                    if branch_name != branch and branch_name not in relatives:
-                        relatives.append(branch_name)
+        for line in lines:
+            match = EXTRACT_BRANCH_NAME.match(line)
+            if match:
+                branch_name = match.group(1)
+                if branch_name != branch and branch_name not in relatives:
+                    relatives.append(branch_name)
 
         return relatives
 
