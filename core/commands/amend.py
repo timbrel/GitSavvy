37a0fcd3df5c6d980eec004ba3e5f69fb2d9abf7
herr kaste <herr.kaste@gmail.com>
2020-03-25 20:36:42 +0100

    Snake case and define `__all__` for `amend.py`

--- core/commands/amend.py
+++ core/commands/amend.py
@@ -3,12 +3,18 @@ from sublime_plugin import WindowCommand
 from ..git_command import GitCommand
 
 
-class GsAmendCommand(WindowCommand, GitCommand):
+__all__ = (
+    "gs_amend",
+    "gs_quick_stage_current_file_and_amend",
+)
+
+
+class gs_amend(WindowCommand, GitCommand):
     def run(self):
         self.window.run_command("gs_commit", {"amend": True})
 
 
-class GsQuickStageCurrentFileAndAmendCommand(GsAmendCommand, GitCommand):
+class gs_quick_stage_current_file_and_amend(gs_amend, GitCommand):
     def run(self):
         self.git("add", "--", self.file_path)
         self.window.status_message("staged {}".format(self.get_rel_path(self.file_path)))
