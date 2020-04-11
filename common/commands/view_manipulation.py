e75de7773fffd81c4019a3872f0a07983a5fe6a7
herr kaste <herr.kaste@gmail.com>
2020-04-11 18:40:22 +0200

    Remove unused "gs_insert_text_at_cursor" command

--- common/commands/view_manipulation.py
+++ common/commands/view_manipulation.py
@@ -3,25 +3,6 @@ from sublime_plugin import TextCommand
 from ...core.settings import GitSavvySettings
 
 
-class gs_insert_text_at_cursor(TextCommand):
-
-    """
-    Insert the provided text at the current cursor position(s).
-    """
-
-    def run(self, edit, text):
-        text_len = len(text)
-        selected_ranges = []
-
-        for region in self.view.sel():
-            selected_ranges.append((region.begin(), region.end()))
-            self.view.replace(edit, region, text)
-
-        self.view.sel().clear()
-        self.view.sel().add_all([sublime.Region(begin + text_len, end + text_len)
-                                 for begin, end in selected_ranges])
-
-
 class gs_replace_region(TextCommand):
 
     """
