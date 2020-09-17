timbrel/GitSavvy

commit f29a0c95b7436e06e5ca9f8b9b16fd1acf5e8daa

Do not render dashboards twice on resurrect
On resurrect, for example starting up Sublime with the status dashboard
open, we first `focus_view` in `__new__` which will run `render`
`on_activated`, then we `render` a second time in `__init__`.

This patch removes the (imperative) call in `__init__`.

common/ui.py
@@ -72,7 +72,6 @@ class Interface():
 
         if view:
             self.view = view
-            self.render(nuke_cursors=False)
         else:
             self.create_view(repo_path)
             sublime.set_timeout_async(self.on_new_dashboard, 0)

commit 883f565baeac588918e47ada2fa3e9ac37b5701e

Ensure dashboard views render on focus
Fixes #1363

We have two code paths actually when creating a new interface object
for an existing view.

1. The user uses a short-cut or any other binding to open a dashboard.
Then we reuse a possible given view using `focus_view` which in turn
fires `on_activated`.

2. The user just focuses such a view.  Then we receive an `on_activated`
event, and must ensure we `render` imperatively.

This patch here addresses 2.

The bug was introduced by f29a0c9 (Do not render dashboards twice on
resurrect).  Just reverting is not a fix but would lead the other bug
(rendering twice) open again.

common/ui.py
@@ -6,6 +6,7 @@ import sublime
 from sublime_plugin import TextCommand
 
 from . import util
+from ..core.runtime import enqueue_on_worker
 from ..core.settings import GitSavvySettings
 from ..core.utils import focus_view
 
@@ -302,19 +303,19 @@ class GsInterfaceRefreshCommand(TextCommand):
     """
 
     def run(self, edit, nuke_cursors=False):
-        sublime.set_timeout_async(self.run_async, 0)
-        self.nuke_cursors = nuke_cursors
+        enqueue_on_worker(self.run_async, nuke_cursors)
 
-    def run_async(self):
+    def run_async(self, nuke_cursors):
+        # type: (bool) -> None
         interface_type = self.view.settings().get("git_savvy.interface")
-        for InterfaceSubclass in subclasses:
-            if InterfaceSubclass.interface_type == interface_type:
-                existing_interface = interfaces.get(self.view.id(), None)
-                if existing_interface:
-                    existing_interface.render(nuke_cursors=self.nuke_cursors)
-                else:
-                    interface = InterfaceSubclass(view=self.view)
-                    interfaces[interface.view.id()] = interface
+        for cls in subclasses:
+            if cls.interface_type == interface_type:
+                vid = self.view.id()
+                interface = interfaces.get(vid, None)
+                if not interface:
+                    interface = interfaces[vid] = cls(view=self.view)
+                interface.render(nuke_cursors=nuke_cursors)  # type: ignore[union-attr]
+                break
 
 
 class GsInterfaceToggleHelpCommand(TextCommand):