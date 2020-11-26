529bd82f8f93a5cb0dc256d6bd050e0549d676e0
herr kaste <herr.kaste@gmail.com>
2020-11-26 13:03:04 +0100

    Remove unused `get_indexed()` and `IndexedEntry`
    This code, introduced in b602cf85, actually the 4th commit of the
    project, was never used.

--- core/git_mixins/status.py
+++ core/git_mixins/status.py
@@ -22,18 +22,6 @@ else:
 
 FileStatus = namedtuple("FileStatus", ("path", "path_alt", "index_status", "working_status"))
 
-IndexedEntry = namedtuple("IndexEntry", (
-    "src_path",
-    "dst_path",
-    "src_mode",
-    "dst_mode",
-    "src_hash",
-    "dst_hash",
-    "status",
-    "status_score"
-))
-IndexedEntry.__new__.__defaults__ = (None, ) * 8
-
 
 class StatusMixin(mixin_base):
 
@@ -66,57 +54,6 @@ class StatusMixin(mixin_base):
 
         lines = self._get_status()
         return self._parse_status_for_file_statuses(lines)
-
-    def _get_indexed_entry(self, raw_entry):
-        """
-        Parse a diff-index entry into an IndexEntry object.  Each input entry
-        will have either three NUL-separated fields if the file has been renamed,
-        and two if it has not been renamed.
-
-        The first field will always contain the meta_data related to the field,
-        which includes: original and new file-system mode, the original and new
-        git object-hashes, and a status letter indicating the nature of the
-        change.
-        """
-        parts = [part for part in raw_entry.split("\x00") if part]
-        if len(parts) == 2:
-            meta_data, src_path = parts
-            dst_path = src_path
-        elif len(parts) == 3:
-            meta_data, src_path, dst_path = parts
-
-        src_mode, dst_mode, src_hash, dst_hash, status = meta_data
-
-        status_score = status[1:]
-        if status_score:
-            status = status[0]
-
-        return IndexedEntry(
-            src_path,
-            dst_path,
-            src_mode,
-            dst_mode,
-            src_hash,
-            dst_hash,
-            status,
-            status_score
-        )
-
-    def get_indexed(self):
-        """
-        Return a list of `IndexEntry`s.  Each entry in the list corresponds
-        to a file that 1) is in HEAD, and 2) is staged with changes.
-        """
-        # Return an entry for each file with a difference between HEAD and its
-        # counterpart in the current index.  Entries will be separated by `:` and
-        # each field will be separated by NUL charachters.
-        stdout = self.git("diff-index", "-z", "--cached", "HEAD")
-
-        return [
-            self._get_indexed_entry(raw_entry)
-            for raw_entry in stdout.split(":")
-            if raw_entry
-        ]
 
     def sort_status_entries(self, file_status_list):
         """
