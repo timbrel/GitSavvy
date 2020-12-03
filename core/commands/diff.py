timbrel/GitSavvy

commit 4060ae796e47f6fcd94b06d41e1c2baee96b880b

Other: improve catching errors

core/git_command.py
@@ -9,6 +9,7 @@ Define a base command class that:
 import os
 import subprocess
 import shutil
+from contextlib import contextmanager
 
 import sublime
 
@@ -97,19 +98,15 @@ class GitCommand(StatusMixin,
 
         stdout, stderr = None, None
 
-        def raise_error(msg):
-            if type(msg) == str and "fatal: Not a git repository" in msg:
+        try:
+            if not working_dir:
+                working_dir = self.repo_path
+        except Exception as e:
+            # offer initialization when "Not a git repository" is thrown from self.repo_path
+            if type(e) == ValueError and e.args and "Not a git repository" in e.args[0]:
                 sublime.set_timeout_async(
                     lambda: sublime.active_window().run_command("gs_offer_init"))
-
-            elif type(msg) == str and "*** Please tell me who you are." in msg:
-                sublime.set_timeout_async(
-                    lambda: sublime.active_window().run_command("gs_setup_user"))
-
-            sublime.status_message(
-                "Failed to run `git {}`. See log for details.".format(command[1])
-            )
-            raise GitSavvyError(msg)
+            raise GitSavvyError(e)
 
         try:
             startupinfo = None
@@ -124,7 +121,7 @@ class GitCommand(StatusMixin,
                                  stdin=subprocess.PIPE,
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE,
-                                 cwd=working_dir or self.repo_path,
+                                 cwd=working_dir,
                                  env=environ,
                                  startupinfo=startupinfo)
             stdout, stderr = p.communicate((stdin.encode(encoding=stdin_encoding) if encode else stdin) if stdin else None)
@@ -132,7 +129,8 @@ class GitCommand(StatusMixin,
                 stdout, stderr = self.decode_stdout(stdout, savvy_settings), stderr.decode()
 
         except Exception as e:
-            raise_error(e)
+            # this should never be reached
+            raise GitSavvyError("Please report this error to GitSavvy:\n\n{}".format(e))
 
         finally:
             end = time.time()
@@ -147,8 +145,16 @@ class GitCommand(StatusMixin,
                     end - start
                 )
 
-        if not p.returncode == 0 and throw_on_stderr:
-            raise_error("`{}` failed with following output:\n{}\n{}".format(
+        if throw_on_stderr and not p.returncode == 0:
+            sublime.status_message(
+                "Failed to run `git {}`. See log for details.".format(command[1])
+            )
+
+            if "*** Please tell me who you are." in stderr:
+                sublime.set_timeout_async(
+                    lambda: sublime.active_window().run_command("gs_setup_user"))
+
+            raise GitSavvyError("`{}` failed with following output:\n{}\n{}".format(
                 command_str, stdout, stderr
             ))
 
@@ -227,7 +233,7 @@ class GitCommand(StatusMixin,
 
         return None
 
-    def find_repo_path(self, throw_on_stderr=False):
+    def find_repo_path(self):
         """
         Similar to find_working_dir, except that it does not stop on the first
         directory found, rather on the first git repository found.
@@ -248,7 +254,7 @@ class GitCommand(StatusMixin,
                 folders = window.folders()
                 if folders and os.path.isdir(folders[0]):
                     repo_path = self.find_git_toplevel(
-                        folders[0], throw_on_stderr=throw_on_stderr)
+                        folders[0], throw_on_stderr=False)
 
         return os.path.realpath(repo_path) if repo_path else None
 
@@ -264,23 +270,10 @@ class GitCommand(StatusMixin,
 
     @property
     def repo_path(self):
-        return self._repo_path()
-
-    @property
-    def short_repo_path(self):
-        if "HOME" in os.environ:
-            return self.repo_path.replace(os.environ["HOME"], "~")
-        else:
-            return self.repo_path
-
-    def _repo_path(self, throw_on_stderr=True):
         """
         Return the absolute path to the git repo that contains the file that this
         view interacts with.  Like `file_path`, this can be overridden by setting
         the view's `git_savvy.repo_path` setting.
-
-        Do not raise error when throw_on_stderr is `False`. It is needed
-        in GsUpdateStatusBarCommand, otherwise, spurious popup will be shown.
         """
         # The below condition will be true if run from a WindowCommand and false
         # from a TextCommand.
@@ -288,14 +281,13 @@ class GitCommand(StatusMixin,
         repo_path = view.settings().get("git_savvy.repo_path") if view else None
 
         if not repo_path or not os.path.exists(repo_path):
-            repo_path = self.find_repo_path(throw_on_stderr=throw_on_stderr)
+            repo_path = self.find_repo_path()
             if not repo_path:
-                # don't throw error for detached view
-                view_is_detached = view and not view.window()
-                if not view_is_detached and throw_on_stderr:
-                    raise ValueError("Unable to determine Git repo path.")
+                window = view.window()
+                if window and window.folders():
+                    raise ValueError("Not a git repository.")
                 else:
-                    return None
+                    raise ValueError("Unable to determine Git repo path.")
 
             if view:
                 file_name = view.file_name()
@@ -304,6 +296,13 @@ class GitCommand(StatusMixin,
                     view.settings().set("git_savvy.repo_path", repo_path)
 
         return os.path.realpath(repo_path) if repo_path else repo_path
+
+    @property
+    def short_repo_path(self):
+        if "HOME" in os.environ:
+            return self.repo_path.replace(os.environ["HOME"], "~")
+        else:
+            return self.repo_path
 
     @property
     def file_path(self):

core/commands/status_bar.py
@@ -53,14 +53,15 @@ class GsUpdateStatusBarCommand(TextCommand, GitCommand):
             last_execution = int(round(time.time() * 1000))
 
     def run_async(self):
-        # Short-circuit update attempts for files not part of Git repo.
-        if not self._repo_path(throw_on_stderr=False):
-            self.view.erase_status("gitsavvy-repo-status")
-            return
-
+        # disable logging and git raise error
         with debug.disable_logging():
-            short_status = self.get_branch_status_short()
-        self.view.set_status("gitsavvy-repo-status", short_status)
+            # ignore all other possible errors
+            try:
+                self.repo_path  # check for ValueError
+                short_status = self.get_branch_status_short()
+                self.view.set_status("gitsavvy-repo-status", short_status)
+            except Exception as e:
+                self.view.erase_status("gitsavvy-repo-status")
 
         global update_status_bar_soon
         update_status_bar_soon = False

commit 5fbf7b10411a1e40bdee7761d8e8815e4e10cf92

Fix: Let the diff view catch decode errors again
Regression since 4060ae79 (Other: improve catching errors, Jul 2017)

The diff view catched a `GitSavvyError` and looked for a
`UnicodeDecodeError` in its first argument.  This is never the case
since the refactoring in 4060ae79.

Opt-out of automatic decoding using `decode=False`.  Instead of showing
a modal dialog in case of an error, just print a message in place of
the usual diff.

core/commands/diff.py
@@ -14,7 +14,7 @@ from . import intra_line_colorizer
 from .navigate import GsNavigate
 from ..fns import filter_, flatten
 from ..parse_diff import SplittedDiff
-from ..git_command import GitCommand, GitSavvyError
+from ..git_command import GitCommand
 from ..runtime import enqueue_on_ui, enqueue_on_worker
 from ..utils import flash, focus_view, line_indentation
 from ..view import replace_view_content, Position
@@ -54,6 +54,16 @@ else:
 
 DIFF_TITLE = "DIFF: {}"
 DIFF_CACHED_TITLE = "DIFF: {} (staged)"
+DECODE_ERROR_MESSAGE = """
+-- Can't decode diff output. --
+
+You may have checked in binary data git doesn't detect, or not UTF-8
+encoded files. In the latter case use the "fallback_encoding" setting,
+in the former you may want to edit the `.gitattributes` file.
+
+Note, however, that diffs with *mixed* encodings are not supported.
+"""
+
 
 # Clickable lines:
 # (A)  common/commands/view_manipulation.py  |   1 +
@@ -186,8 +196,6 @@ class gs_diff_refresh(TextCommand, GitCommand):
             return
 
         settings = view.settings()
-        if settings.get("git_savvy.disable_diff"):
-            return
         repo_path = settings.get("git_savvy.repo_path")
         file_path = settings.get("git_savvy.file_path")
         in_cached_mode = settings.get("git_savvy.diff_view.in_cached_mode")
@@ -233,27 +241,26 @@ class gs_diff_refresh(TextCommand, GitCommand):
 
         prelude += "\n--\n"
 
+        raw_diff = self.git(
+            "diff",
+            "--ignore-all-space" if ignore_whitespace else None,
+            "--unified={}".format(context_lines) if context_lines is not None else None,
+            "--stat" if show_diffstat else None,
+            "--patch",
+            "--no-color",
+            "--cached" if in_cached_mode else None,
+            base_commit,
+            target_commit,
+            "--",
+            file_path,
+            decode=False
+        )
+
+        encodings = self.get_encoding_candidates()
         try:
-            diff = self.git(
-                "diff",
-                "--ignore-all-space" if ignore_whitespace else None,
-                "--unified={}".format(context_lines) if context_lines is not None else None,
-                "--stat" if show_diffstat else None,
-                "--patch",
-                "--no-color",
-                "--cached" if in_cached_mode else None,
-                base_commit,
-                target_commit,
-                "--", file_path)
-        except GitSavvyError as err:
-            # When the output of the above Git command fails to correctly parse,
-            # the expected notification will be displayed to the user.  However,
-            # once the userpresses OK, a new refresh event will be triggered on
-            # the view. Prevent refreshing again by setting a flag.
-            if err.args and type(err.args[0]) == UnicodeDecodeError:
-                settings.set("git_savvy.disable_diff", True)
-                return
-            raise err
+            diff, _ = self.try_decode(raw_diff, encodings, show_modal_on_error=False)
+        except UnicodeDecodeError:
+            diff = DECODE_ERROR_MESSAGE
 
         if settings.get("git_savvy.just_committed"):
             if diff:

core/git_command.py
@@ -271,16 +271,17 @@ class _GitCommand(SettingsMixin):
         decoded, _ = self.try_decode(stdout, encodings)
         return decoded
 
-    def try_decode(self, input, encodings):  # type: ignore  # missing return statement
-        # type: (bytes, Sequence[str]) -> Tuple[str, str]
-        assert encodings
+    def try_decode(self, input, encodings, show_modal_on_error=True):
+        # type: (bytes, Sequence[str], bool) -> Tuple[str, str]
         for n, encoding in enumerate(encodings, start=1):
             try:
                 return input.decode(encoding), encoding
             except UnicodeDecodeError as err:
                 if n == len(encodings):
-                    sublime.error_message(FALLBACK_PARSE_ERROR_MSG)
+                    if show_modal_on_error:
+                        sublime.error_message(FALLBACK_PARSE_ERROR_MSG)
                     raise err
+        assert False  # no silent fall-through
 
     @property
     def encoding(self):

syntax/show_commit.sublime-syntax
@@ -19,6 +19,8 @@ contexts:
       scope: meta.commit-header-and-stat-splitter
       embed: "scope:git-savvy.diff"
       escape: (?=^commit \h{7,})
+    - match: ^-- .* --$
+      scope: markup.deleted
 
   commit-header:
     - meta_scope: meta.commit-info.header

tests/test_diff_view.py
@@ -499,7 +499,7 @@ class TestZooming(DeferrableTestCase):
 
         view.settings().set('git_savvy.diff_view.context_lines', CONTEXT_LINES)
         cmd = module.gs_diff_refresh(view)
-        when(cmd).git(...).thenReturn('NEW CONTENT')
+        when(cmd).git(...).thenReturn(b'NEW CONTENT')
 
         cmd.run({'unused_edit'})
         verify(cmd).git('diff', None, FLAG, ...)
@@ -591,7 +591,7 @@ class TestDiffView(DeferrableTestCase):
         REPO_PATH = '/not/there'
         DIFF = fixture('diff_1.txt')
 
-        when(gs_diff_refresh).git('diff', ...).thenReturn(DIFF)
+        when(gs_diff_refresh).git('diff', ...).thenReturn(DIFF.encode())
         cmd = gs_diff(self.window)
         when(cmd).get_repo_path().thenReturn(REPO_PATH)
         cmd.run()
@@ -617,7 +617,7 @@ class TestDiffView(DeferrableTestCase):
         REPO_PATH = '/not/there'
         DIFF = fixture('diff_1.txt')
 
-        when(gs_diff_refresh).git('diff', ...).thenReturn(DIFF)
+        when(gs_diff_refresh).git('diff', ...).thenReturn(DIFF.encode())
         cmd = gs_diff(self.window)
         when(cmd).get_repo_path().thenReturn(REPO_PATH)
         cmd.run()