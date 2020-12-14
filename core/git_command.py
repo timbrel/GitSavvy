ea40d3d000e3ccd979f58454a217d6369e4787cc
herr kaste <herr.kaste@gmail.com>
2020-12-14 23:25:57 +0100

    Remove `show_input_in_output` setting
    The setting is a misnomer as it's actually more like "show command in
    panel".  Anyway, showing the command we execute is part of our design
    and not changeable.  Remove the setting which defaulted to `True`
    anyway.

--- core/git_command.py
+++ core/git_command.py
@@ -186,8 +186,7 @@ class _GitCommand(SettingsMixin):
                 util.log.panel("", run_async=False)  # clear panel
                 if self.savvy_settings.get("show_stdin_in_output") and stdin is not None:
                     util.log.panel_append("STDIN\n{}\n".format(stdin), run_async=False)
-                if self.savvy_settings.get("show_input_in_output"):
-                    util.log.panel_append("$ {}\n".format(command_str), run_async=False)
+                util.log.panel_append("$ {}\n".format(command_str), run_async=False)
 
                 _log = partial(util.log.panel_append, run_async=False)
                 log_b = lambda line: _log(line.decode())
