db4e5fd54fd34abc2ff58a893e1215d3af531a51
herr kaste <herr.kaste@gmail.com>
2020-12-18 17:29:09 +0100

    Extract `some_window()` method

--- core/settings.py
+++ core/settings.py
@@ -90,13 +90,17 @@ class SettingsMixin:
     @property
     def savvy_settings(self):
         if not self._savvy_settings:
-            window = (
-                maybe(lambda: self.window)  # type: ignore[attr-defined]
-                or maybe(lambda: self.view.window())  # type: ignore[attr-defined]
-                or sublime.active_window()
-            )
+            window = self.some_window()
             self._savvy_settings = GitSavvySettings(window)
         return self._savvy_settings
+
+    def some_window(self):
+        # type: () -> sublime.Window
+        return (
+            maybe(lambda: self.window)  # type: ignore[attr-defined]
+            or maybe(lambda: self.view.window())  # type: ignore[attr-defined]
+            or sublime.active_window()
+        )
 
 
 def maybe(fn):
