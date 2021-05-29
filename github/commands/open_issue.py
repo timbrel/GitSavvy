8bfba7254bc3038277fbb1aea1436a4b7deb2b78
herr kaste <herr.kaste@gmail.com>
2021-05-29 22:55:03 +0200

    Show tooltip when hovering over an issue

--- github/commands/open_issue.py
+++ github/commands/open_issue.py
@@ -1,6 +1,7 @@
 from webbrowser import open as open_in_browser
 
-from sublime_plugin import TextCommand
+import sublime
+from sublime_plugin import EventListener, TextCommand
 
 from GitSavvy.core.utils import flash
 from GitSavvy.core.git_command import GitCommand
@@ -9,6 +10,7 @@ from .. import github, git_mixins
 
 __all__ = (
     "gs_github_open_issue_at_cursor",
+    "gs_github_hover_on_issues_controller",
 )
 
 
@@ -16,18 +18,48 @@ ISSUE_SCOPES = "constant.other.issue-ref.git-savvy, string.other.issue.git-savvy
 
 
 class gs_github_open_issue_at_cursor(TextCommand, git_mixins.GithubRemotesMixin, GitCommand):
-    def run(self, edit):
+    def run(self, edit, point=None, open_popup=False):
         view = self.view
-        cursor = view.sel()[0].begin()
-        if not view.match_selector(cursor, ISSUE_SCOPES):
+        if point is None:
+            point = view.sel()[0].begin()
+
+        if not view.match_selector(point, ISSUE_SCOPES):
             flash(view, "Not on an issue or pr name.")
             return
 
-        issue_nr = view.substr(view.extract_scope(cursor))[1:]
+        def on_navigate(href: str):
+            open_in_browser(href)
 
+        issue_nr = view.substr(view.extract_scope(point))[1:]
+        url = self.url_for_issue(issue_nr)
+
+        if open_popup:
+            view.show_popup(
+                '<a href="{url}">{url}</a>'.format(url=url),
+                flags=sublime.HIDE_ON_MOUSE_MOVE_AWAY,
+                location=point,
+                max_width=1000,
+                on_navigate=on_navigate
+            )
+        else:
+            open_in_browser(url)
+
+    def url_for_issue(self, issue_nr: str) -> str:
         remotes = self.get_remotes()
         base_remote_name = self.get_integrated_remote_name(remotes)
         base_remote_url = remotes[base_remote_name]
         base_remote = github.parse_remote(base_remote_url)
-        url = "{}/issues/{}".format(base_remote.url, issue_nr)
-        open_in_browser(url)
+        return "{}/issues/{}".format(base_remote.url, issue_nr)
+
+
+class gs_github_hover_on_issues_controller(EventListener):
+    def on_hover(self, view, point, hover_zone):
+        # type: (sublime.View, int, int) -> None
+        if (
+            hover_zone == sublime.HOVER_TEXT
+            and view.match_selector(point, ISSUE_SCOPES)
+        ):
+            view.run_command("gs_github_open_issue_at_cursor", {
+                "point": point,
+                "open_popup": True
+            })
