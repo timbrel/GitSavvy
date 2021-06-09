a9cabaea3cfd2867c05502397b07369f10f42ea1
herr kaste <herr.kaste@gmail.com>
2021-06-09 23:52:16 +0200

    Select the protocol automatically
    Just as when reviewing PRs, select the protocol "ssh" or "https"
    automatically by looking at what the user used for the integrated
    remote.

--- github/commands/create_fork.py
+++ github/commands/create_fork.py
@@ -5,6 +5,7 @@ from ...common import util
 from ...core.git_command import GitCommand
 from .. import github, git_mixins
 
+
 START_CREATE_MESSAGE = "Forking {repo} ..."
 END_CREATE_MESSAGE = "Fork created successfully."
 
@@ -25,24 +26,19 @@ class GsGithubCreateForkCommand(
         sublime.set_timeout_async(self.run_async, 0)
 
     def run_async(self):
-        base_remote = github.parse_remote(self.get_integrated_remote_url())
+        remotes = self.get_remotes()
+        base_remote_name = self.get_integrated_remote_name(remotes)
+        base_remote_url = remotes[base_remote_name]
+        base_remote = github.parse_remote(base_remote_url)
+
         self.window.status_message(START_CREATE_MESSAGE.format(repo=base_remote.url))
         result = github.create_fork(base_remote)
-        self.clone_url = result["clone_url"] if "clone_url" in result else None
-        self.ssh_url = result["ssh_url"] if "ssh_url" in result else None
         self.window.status_message(END_CREATE_MESSAGE)
         util.debug.add_to_log(("github: fork result:\n{}".format(result)))
-        self.window.show_quick_panel(
-            ["Add fork as remote?",
-             self.clone_url,
-             self.ssh_url],
-            self.on_select_action
-        )
 
-    def on_select_action(self, idx):
-        if idx == 0:
-            return
-        elif idx == 1:
-            self.window.run_command("gs_remote_add", {"url": self.clone_url})
-        elif idx == 2:
-            self.window.run_command("gs_remote_add", {"url": self.ssh_url})
+        url = (
+            result["ssh_url"]
+            if base_remote_url.startswith("git@")
+            else result["clone_url"]
+        )
+        self.window.run_command("gs_remote_add", {"url": url})
