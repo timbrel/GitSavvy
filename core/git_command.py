timbrel/GitSavvy

commit a5561c5932fe582b93500583ba29a5927f421043

Fallback to first folder only if necessary

core/git_command.py
@@ -138,6 +138,11 @@ def search_for_git(folder):
         return None
 
 
+def is_subpath(topfolder, path):
+    # type: (str, str) -> bool
+    return os.path.commonprefix([topfolder, path]) == topfolder
+
+
 class _GitCommand(SettingsMixin):
 
     """
@@ -407,7 +412,11 @@ class _GitCommand(SettingsMixin):
             if window:
                 folders = window.folders()
                 if folders:
-                    yield folders[0]
+                    if (
+                        not file_name
+                        or not is_subpath(resolve_path(folders[0]), resolve_path(file_name))
+                    ):
+                        yield folders[0]
 
         return filter(os.path.isdir, __search_paths())
 

commit 1eaacd2352b3d324e072c56e4d6853cc5d6254b9

Test if the reported filename is accessible
As an edge case, for example when viewing a file in a zip container,
Sublime Text might return a "correct" looking file path although the
later `resolve_path` will throw as the file does not exist.

That case was generally handled by a later call to `os.isdir`.  It is
only within this subroutine we might use the invalid filename.

Bug introduced by a5561c59 (Fallback to first folder only if necessary).

core/git_command.py
@@ -436,6 +436,8 @@ class _GitCommand(SettingsMixin):
         def __search_paths():
             # type: () -> Iterator[str]
             file_name = self._current_filename()
+            if file_name and not os.path.isfile(file_name):
+                file_name = None
             if file_name:
                 yield os.path.dirname(file_name)
 