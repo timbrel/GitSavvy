bcad564080414004ba694fd7005cd798baabd12b
herr kaste <herr.kaste@gmail.com>
2022-09-08 15:49:06 +0200

    Remember last used branch used for `on` or `onto`

--- core/store.py
+++ core/store.py
@@ -17,6 +17,7 @@ if MYPY:
         {
             "status": WorkingDirState,
             "last_branches": Deque[Optional[str]],
+            "last_local_branch_for_rebase": Optional[str],
             "last_remote_used": Optional[str],
             "last_remote_used_for_push": Optional[str],
             "last_remote_used_with_option_all": Optional[str],
