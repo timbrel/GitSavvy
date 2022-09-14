4a38db92103026c81c69b5c6e85bfad779ae3491
herr kaste <herr.kaste@gmail.com>
2022-09-14 21:29:16 +0200

    Add tests

--- tests/test_git_mixins.py
+++ tests/test_git_mixins.py
@@ -3,6 +3,7 @@ from GitSavvy.tests.mockito import unstub, when
 from GitSavvy.tests.parameterized import parameterized as p, param
 
 from GitSavvy.core.git_command import GitCommand
+from GitSavvy.core import git_mixins
 
 
 class TestGitMixinsUsage(DeferrableTestCase):
@@ -46,3 +47,85 @@ class TestFetchInterface(TestGitMixinsUsage):
         repo = GitCommand()
         when(repo).git("fetch", "--prune", ...)
         self.assertRaises(TypeError, lambda: repo.fetch(*parameters.args, **parameters.kwargs))
+
+
+sha_and_subject = "\x0089b79cd737465ed308ecc00289d00a6f923f2da5\x00The Subject"
+
+
+class TestGetBranchesParsing(TestGitMixinsUsage):
+    def test_local_branch(self):
+        repo = GitCommand()
+        git_output = " \x00refs/heads/master\x00refs/remotes/origin/master\x00" + sha_and_subject
+        when(repo).git("for-each-ref", ...).thenReturn(git_output)
+        actual = list(repo.get_branches())
+        self.assertEqual(actual, [
+            git_mixins.branches.Branch(
+                "master",
+                None,
+                "master",
+                "89b79cd737465ed308ecc00289d00a6f923f2da5",
+                "The Subject",
+                "origin/master",
+                "",
+                False,
+                "",
+            )
+        ])
+
+    def test_active_local_branch(self):
+        repo = GitCommand()
+        git_output = "*\x00refs/heads/master\x00refs/remotes/origin/master\x00" + sha_and_subject
+        when(repo).git("for-each-ref", ...).thenReturn(git_output)
+        actual = list(repo.get_branches())
+        self.assertEqual(actual, [
+            git_mixins.branches.Branch(
+                "master",
+                None,
+                "master",
+                "89b79cd737465ed308ecc00289d00a6f923f2da5",
+                "The Subject",
+                "origin/master",
+                "",
+                True,
+                "",
+            )
+        ])
+
+    def test_remote_branch(self):
+        repo = GitCommand()
+        git_output = " \x00refs/remotes/origin/dev\x00\x00" + sha_and_subject
+        when(repo).git("for-each-ref", ...).thenReturn(git_output)
+        actual = list(repo.get_branches())
+        self.assertEqual(actual, [
+            git_mixins.branches.Branch(
+                "dev",
+                "origin",
+                "origin/dev",
+                "89b79cd737465ed308ecc00289d00a6f923f2da5",
+                "The Subject",
+                "",
+                "",
+                False,
+                "",
+            )
+        ])
+
+    def test_tracking_status(self):
+        repo = GitCommand()
+        git_output = " \x00refs/heads/master\x00refs/remotes/origin/master\x00[gone]" + sha_and_subject
+        when(repo).git("for-each-ref", ...).thenReturn(git_output)
+        actual = list(repo.get_branches())
+        self.assertEqual(actual, [
+            git_mixins.branches.Branch(
+                "master",
+                None,
+                "master",
+                "89b79cd737465ed308ecc00289d00a6f923f2da5",
+                "The Subject",
+                "origin/master",
+                "gone",
+                False,
+                "",
+            )
+        ])
+
