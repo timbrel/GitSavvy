f356cfa7b197c2e9cc995f60882dbc1573989892
herr kaste <herr.kaste@gmail.com>
2022-09-18 00:47:46 +0200

    Add types to `tag.py`

--- core/commands/tag.py
+++ core/commands/tag.py
@@ -6,6 +6,19 @@ from ..git_command import GitCommand
 from ..ui_mixins.quick_panel import PanelActionMixin
 from ..ui_mixins.input_panel import show_single_line_input_panel
 
+MYPY = False
+if MYPY:
+    from typing import Literal, Optional
+    ReleaseTypes = Literal[
+        "patch",
+        "minor",
+        "major",
+        "prerelease",
+        "prepatch",
+        "preminor",
+        "premajor"]
+
+
 RELEASE_REGEXP = re.compile(r"^([0-9A-Za-z-]*[A-Za-z-])?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-\.]*?)?([0-9]+))?$")
 
 TAG_CREATE_PROMPT = "Enter tag:"
@@ -14,6 +27,7 @@ TAG_CREATE_MESSAGE_PROMPT = "Enter message:"
 
 
 def smart_incremented_tag(tag, release_type):
+    # type: (str, ReleaseTypes) -> Optional[str]
     """
     Automatic increment of a given tag depending on the type of release.
     """
@@ -58,6 +72,7 @@ class GsTagCreateCommand(TextCommand, GitCommand):
     """
 
     def run(self, edit, tag_name="", target_commit=None):
+        # type: (object, str, str) -> None
         window = self.view.window()
         if not window:
             return
@@ -66,6 +81,7 @@ class GsTagCreateCommand(TextCommand, GitCommand):
         show_single_line_input_panel(TAG_CREATE_PROMPT, tag_name, self.on_entered_name)
 
     def on_entered_name(self, tag_name):
+        # type: (str) -> None
         """
         After the user has entered a tag name, validate the tag name and prompt for
         a tag message.
@@ -95,6 +111,7 @@ class GsTagCreateCommand(TextCommand, GitCommand):
         )
 
     def on_entered_message(self, message):
+        # type: (str) -> None
         """
         Create a tag with the specified tag name and message.
         """
@@ -124,6 +141,7 @@ class GsSmartTagCommand(PanelActionMixin, TextCommand):
     ]
 
     def smart_tag(self, release_type):
+        # type: (ReleaseTypes) -> None
         tag_name = ""
         last_tag_name = self.get_last_local_semver_tag()
         if last_tag_name:
