ec325e171320885dcc16533cb73a78b8d6c56cce
herr kaste <herr.kaste@gmail.com>
2022-09-21 12:10:51 +0200

    Allow better subclassing of classes using `WithProvideWindow`
    Here `super(WithProvideWindow, cloned).run_(...)` is not flexible.
    Naturally we want just `super()` to let python handle the mru but
    can't because we want to run that on the clone.
    
    Just don't use `super` and call `run_` manually which runs from top to
    bottom again.  To restrict the recursion check the presence of
    `self.window`.

--- core/base_commands.py
+++ core/base_commands.py
@@ -27,21 +27,24 @@ class WithProvideWindow:
     window = None  # type: sublime.Window
 
     def run_(self, edit_token, args):
-        window = self.view.window()  # type: ignore[attr-defined]
-        if not window:
-            raise RuntimeError(
-                "Assertion failed! "
-                "'{}' is already detached".format(self.view)
-            )
         # Sublime instantiates `TextCommand`s for each view once.  Moving
         # a view to another window creates a new view.
         # We want to make sure that `self` is a unique context even when we
         # defer to the other threads.  T.i. `self` is unique, stable and can
         # be used as a store for *one* call.  `self` cannot be used as a store
         # across *multiple* calls.
-        cloned = self.__class__(self.view)  # type: ignore[attr-defined, call-arg]
-        cloned.window = window
-        return super(WithProvideWindow, cloned).run_(edit_token, args)  # type: ignore[misc]
+        if self.window is None:
+            window = self.view.window()  # type: ignore[unreachable]
+            if not window:
+                raise RuntimeError(
+                    "Assertion failed! "
+                    "'{}' is already detached".format(self.view)
+                )
+            self.window = window
+            return super().run_(edit_token, args)
+        else:
+            cloned = self.__class__(self.view)  # type: ignore[attr-defined, call-arg]
+            return cloned.run_(edit_token, args)
 
 
 class WithInputHandlers:
