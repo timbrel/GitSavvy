ee6a046541acdfdc08f1d7fa94940144fb81262a
herr kaste <herr.kaste@gmail.com>
2022-09-28 23:07:32 +0200

    Modernize `add_fork_as_remote.py`

--- github/commands/add_fork_as_remote.py
+++ github/commands/add_fork_as_remote.py
@@ -1,24 +1,26 @@
-import sublime
-from sublime_plugin import WindowCommand
 from itertools import chain
 
-from ...core.git_command import GitCommand
 from ...core.ui_mixins.quick_panel import show_paginated_panel
 from .. import github
 from .. import git_mixins
+from GitSavvy.core.base_commands import GsWindowCommand
+from GitSavvy.core.runtime import on_worker
 
 
-class GsGithubAddForkAsRemoteCommand(WindowCommand, git_mixins.GithubRemotesMixin, GitCommand):
+__all__ = (
+    "gs_github_add_fork_as_remote",
+)
+
+
+class gs_github_add_fork_as_remote(git_mixins.GithubRemotesMixin, GsWindowCommand):
 
     """
     Get list of repos on GitHub associated with the active repo.  Display, and when
     selected, add selection as git remote.
     """
 
+    @on_worker
     def run(self):
-        sublime.set_timeout_async(self.run_async, 0)
-
-    def run_async(self):
         base_remote = github.parse_remote(self.get_integrated_remote_url())
         base_repo_data = github.get_repo_data(base_remote)
         parent = None
@@ -30,13 +32,12 @@ class GsGithubAddForkAsRemoteCommand(WindowCommand, git_mixins.GithubRemotesMixi
 
         if "source" in base_repo_data:
             source = base_repo_data["source"]
-            if not parent["clone_url"] == source["clone_url"]:
+            if parent and parent["clone_url"] != source["clone_url"]:
                 forks.append(source)
 
-        forks = chain(forks, github.get_forks(base_remote))
-
+        forks_ = chain(forks, github.get_forks(base_remote))
         show_paginated_panel(
-            forks,
+            forks_,
             self.on_select_fork,
             limit=self.savvy_settings.get("github_per_page_max", 100),
             format_item=lambda fork: (fork["full_name"], fork),
