4d35b4b3baf62f1d60002a9059bb7c752bc90bd5
herr kaste <herr.kaste@gmail.com>
2022-10-16 22:01:15 +0200

    Type `show_single_line_input_panel`

--- core/ui_mixins/input_panel.py
+++ core/ui_mixins/input_panel.py
@@ -1,8 +1,21 @@
 import sublime
 
 
+MYPY = False
+if MYPY:
+    from typing import Callable, Optional
+    ValueCallback = Callable[[str], None]
+    CancelCallback = Callable[[], None]
+
+
 def show_single_line_input_panel(
-        caption, initial_text, on_done, on_change=None, on_cancel=None, select_text=True):
+    caption,  # type: str
+    initial_text,  # type: str
+    on_done,  # type: ValueCallback
+    on_change=None,  # type: Optional[ValueCallback]
+    on_cancel=None,  # type: Optional[CancelCallback]
+    select_text=True  # type: bool
+):  # type: (...) -> sublime.View
     window = sublime.active_window()
     v = window.show_input_panel(caption, initial_text, on_done, on_change, on_cancel)
     if select_text:
