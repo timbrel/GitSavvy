790b386a97bd5204b75231a933b7892b4bba5304
herr kaste <herr.kaste@gmail.com>
2022-11-17 14:36:39 +0100

    Add revert `--abort`, `--continue`, and `--skip` commands

--- core/commands/revert.py
+++ core/commands/revert.py
@@ -3,10 +3,19 @@ from sublime_plugin import WindowCommand
 from .log import LogMixin
 from ..git_command import GitCommand
 from ...common import util
+from GitSavvy.core.base_commands import GsWindowCommand
+from GitSavvy.core.runtime import on_worker
 
 
-class GsRevertCommitCommand(LogMixin, WindowCommand, GitCommand):
+__all__ = (
+    "gs_revert_commit",
+    "gs_revert_abort",
+    "gs_revert_continue",
+    "gs_revert_skip",
+)
 
+
+class gs_revert_commit(LogMixin, WindowCommand, GitCommand):
     def run_async(self, **kwargs):
         if "commit_hash" in kwargs:
             commit_hash = kwargs["commit_hash"]
@@ -17,3 +26,24 @@ class GsRevertCommitCommand(LogMixin, WindowCommand, GitCommand):
     def do_action(self, commit_hash, **kwargs):
         self.git("revert", commit_hash)
         util.view.refresh_gitsavvy(self.window.active_view(), refresh_sidebar=True)
+
+
+class gs_revert_abort(GsWindowCommand):
+    @on_worker
+    def run(self):
+        self.git("revert", "--abort")
+        util.view.refresh_gitsavvy_interfaces(self.window, refresh_sidebar=True)
+
+
+class gs_revert_continue(GsWindowCommand):
+    @on_worker
+    def run(self):
+        self.git("revert", "--continue")
+        util.view.refresh_gitsavvy_interfaces(self.window, refresh_sidebar=True)
+
+
+class gs_revert_skip(GsWindowCommand):
+    @on_worker
+    def run(self):
+        self.git("revert", "--skip")
+        util.view.refresh_gitsavvy_interfaces(self.window, refresh_sidebar=True)
