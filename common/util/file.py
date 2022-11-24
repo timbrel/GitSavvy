timbrel/GitSavvy

commit 15ebebf23cd9baaea076f4a00e68af2f77670d7a

Use new `find_syntax_for_file` function in ST4
Newer Sublime Text builds have syntax functions so we remove our way
of finding syntaxes for given views.

Note that we keep the `syntax_file_map` generally alive because the
preferred way is still to just take the syntax currently applied
to a (hopefully open) view into a file.  (`view.syntax()`)

But `determine_syntax_files` becomes a noop and thus `syntax_file_map`
is empty at Sublime's startup.

Note that this removes any dependency for recent Sublime Text versions.

common/util/file.py
@@ -1,17 +1,17 @@
 from collections import defaultdict
 from contextlib import contextmanager
 import os
-import plistlib
-import re
-import threading
-import yaml
 
 import sublime
+
+from GitSavvy.core.fns import maybe
 
 
 MYPY = False
 if MYPY:
     from typing import DefaultDict, List, Optional
+
+SUBLIME_HAS_SYNTAX_FUNCTIONS = int(sublime.version()) >= 4080
 
 
 if 'syntax_file_map' not in globals():
@@ -21,69 +21,75 @@ if 'determine_syntax_thread' not in globals():
     determine_syntax_thread = None
 
 
-def determine_syntax_files():
-    # type: () -> None
-    global determine_syntax_thread
-    if not syntax_file_map:
-        determine_syntax_thread = threading.Thread(
-            target=_determine_syntax_files)
-        determine_syntax_thread.start()
+if SUBLIME_HAS_SYNTAX_FUNCTIONS:
+    def determine_syntax_files():
+        # type: () -> None
+        ...
 
+else:
+    import plistlib
+    import re
+    import threading
+    import yaml
 
-def try_parse_for_file_extensions(text):
-    # type: (str) -> Optional[List[str]]
-    match = re.search(r"^file_extensions:\n((.*\n)+?)^(?=\w)", text, re.M)
-    if match:
-        return _try_yaml_parse(match.group(0))
-    return _try_yaml_parse(text)
+    def determine_syntax_files():
+        # type: () -> None
+        global determine_syntax_thread
+        if not syntax_file_map:
+            determine_syntax_thread = threading.Thread(
+                target=_determine_syntax_files)
+            determine_syntax_thread.start()
 
+    def _determine_syntax_files():
+        # type: () -> None
+        handle_tm_language_files()
+        handle_sublime_syntax_files()
 
-def _try_yaml_parse(text):
-    # type: (str) -> Optional[List[str]]
-    try:
-        return yaml.safe_load(text)["file_extensions"]
-    except Exception:
-        return None
+    def handle_tm_language_files():
+        # type: () -> None
+        syntax_files = sublime.find_resources("*.tmLanguage")
+        for syntax_file in syntax_files:
+            try:
+                resource = sublime.load_binary_resource(syntax_file)
+            except Exception:
+                print("GitSavvy: could not load {}".format(syntax_file))
+                continue
 
+            try:
+                extensions = plistlib.readPlistFromBytes(resource).get("fileTypes", [])
+            except Exception:
+                print("GitSavvy: could not parse {}".format(syntax_file))
+                continue
 
-def _determine_syntax_files():
-    # type: () -> None
-    handle_tm_language_files()
-    handle_sublime_syntax_files()
+            for extension in extensions:
+                syntax_file_map[extension].append(syntax_file)
 
+    def handle_sublime_syntax_files():
+        # type: () -> None
+        syntax_files = sublime.find_resources("*.sublime-syntax")
+        for syntax_file in syntax_files:
+            try:
+                resource = sublime.load_resource(syntax_file)
+            except Exception:
+                print("GitSavvy: could not load {}".format(syntax_file))
+                continue
 
-def handle_tm_language_files():
-    # type: () -> None
-    syntax_files = sublime.find_resources("*.tmLanguage")
-    for syntax_file in syntax_files:
+            for extension in try_parse_for_file_extensions(resource) or []:
+                syntax_file_map[extension].append(syntax_file)
+
+    def try_parse_for_file_extensions(text):
+        # type: (str) -> Optional[List[str]]
+        match = re.search(r"^file_extensions:\n((.*\n)+?)^(?=\w)", text, re.M)
+        if match:
+            return _try_yaml_parse(match.group(0))
+        return _try_yaml_parse(text)
+
+    def _try_yaml_parse(text):
+        # type: (str) -> Optional[List[str]]
         try:
-            resource = sublime.load_binary_resource(syntax_file)
+            return yaml.safe_load(text)["file_extensions"]
         except Exception:
-            print("GitSavvy: could not load {}".format(syntax_file))
-            continue
-
-        try:
-            extensions = plistlib.readPlistFromBytes(resource).get("fileTypes", [])
-        except Exception:
-            print("GitSavvy: could not parse {}".format(syntax_file))
-            continue
-
-        for extension in extensions:
-            syntax_file_map[extension].append(syntax_file)
-
-
-def handle_sublime_syntax_files():
-    # type: () -> None
-    syntax_files = sublime.find_resources("*.sublime-syntax")
-    for syntax_file in syntax_files:
-        try:
-            resource = sublime.load_resource(syntax_file)
-        except Exception:
-            print("GitSavvy: could not load {}".format(syntax_file))
-            continue
-
-        for extension in try_parse_for_file_extensions(resource) or []:
-            syntax_file_map[extension].append(syntax_file)
+            return None
 
 
 def guess_syntax_for_file(window, filename):
@@ -114,6 +120,7 @@ def get_syntax_for_file(filename, default="Packages/Text/Plain text.tmLanguage")
     syntaxes = (
         syntax_file_map.get(filename, [])
         or syntax_file_map.get(get_file_extension(filename), [])
+        or maybe(lambda: [sublime.find_syntax_for_file(filename).path])  # type: ignore[union-attr]
         or [default]
     )
     return syntaxes[-1]

stubs/sublime.pyi
@@ -442,3 +442,38 @@ class PhantomSet:
     def __init__(self, view: View, key: str = ...) -> None: ...
     def __del__(self): ...
     def update(self, new_phantoms: Sequence[Phantom]): ...
+
+
+class Syntax:
+    path: str
+    name: str
+    hidden: bool
+    scope: str
+
+def list_syntaxes() -> List[Syntax]:
+    """list all known syntaxes.
+
+    Returns a list of Syntax."""
+
+def syntax_from_path(path: str) -> Optional[Syntax]:
+    """Get the syntax for a specific path.
+
+    Returns a Syntax or None."""
+
+def find_syntax_by_name(name: str) -> list[Syntax]:
+    """Find syntaxes with the specified name.
+
+    Name must match exactly. Return a list of Syntax."""
+
+def find_syntax_by_scope(scope: str) -> list[Syntax]:
+    """Find syntaxes with the specified scope.
+
+    Scope must match exactly. Return a list of Syntax."""
+
+def find_syntax_for_file(path: str, first_line: str = '') -> Optional[Syntax]:
+    """Find the syntax to use for a path.
+
+    Uses the file extension, various application settings and optionally the first line of the file to pick the right syntax for the file.
+
+    Returns a Syntax."""
+

commit 1b34b2e53a073fed7b2be214f7bfaf865a354c5e

Fix not determining a correct syntax at all
Regression since 15ebebf2 (Use new `find_syntax_for_file` function in
ST4).

For ST4 we never start a thread to read all syntaxes so the global
`determine_syntax_thread` stays `None` all the time.  Adjust the
conditional, in fact for ST4 we are always ready to determine a correct
syntax.

common/util/file.py
@@ -115,7 +115,7 @@ def remember_syntax_choice(filename, syntax):
 
 def get_syntax_for_file(filename, default="Packages/Text/Plain text.tmLanguage"):
     # type: (str, str) -> str
-    if not determine_syntax_thread or determine_syntax_thread.is_alive():
+    if not syntax_functions_readied():
         return default
     syntaxes = (
         syntax_file_map.get(filename, [])
@@ -124,6 +124,15 @@ def get_syntax_for_file(filename, default="Packages/Text/Plain text.tmLanguage")
         or [default]
     )
     return syntaxes[-1]
+
+
+def syntax_functions_readied():
+    # type: () -> bool
+    if SUBLIME_HAS_SYNTAX_FUNCTIONS:
+        return True
+    if determine_syntax_thread and not determine_syntax_thread.is_alive():
+        return True
+    return False
 
 
 def get_file_extension(filename):