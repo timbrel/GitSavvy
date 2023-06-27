a25d461655fce7a960a35b23f84c9403f103f817
herr kaste <herr.kaste@gmail.com>
2023-06-27 12:26:41 +0200

    Move `typing` import to the top-level

--- core/fns.py
+++ core/fns.py
@@ -1,11 +1,9 @@
 from functools import partial
 from itertools import accumulate as accumulate_, chain, islice, tee
 
-MYPY = False
-if MYPY:
-    from typing import Callable, Iterable, Iterator, List, Optional, Set, Tuple, TypeVar
-    T = TypeVar('T')
-    U = TypeVar('U')
+from typing import Callable, Iterable, Iterator, List, Optional, Set, Tuple, TypeVar
+T = TypeVar('T')
+U = TypeVar('U')
 
 
 filter_ = partial(filter, None)  # type: Callable[[Iterable[Optional[T]]], Iterator[T]]  # type: ignore[assignment]
