timbrel/GitSavvy

commit e3519a0a838f77196932a15ec9e7ed016d2e0fc2

Move closing-active-view logic up
This follows the code of `inline_diff.py`.

core/commands/diff.py
@@ -109,6 +109,11 @@ def compute_identifier_for_view(view):
     ) if settings.get('git_savvy.diff_view') else None
 
 
+def is_diff_view(view):
+    # type: (sublime.View) -> bool
+    return view.settings().get('git_savvy.diff_view')
+
+
 class gs_diff(WindowCommand, GitCommand):
 
     """
@@ -142,6 +147,13 @@ class gs_diff(WindowCommand, GitCommand):
         active_view = self.window.active_view()
         assert active_view
 
+        if is_diff_view(active_view) and (
+            in_cached_mode is None
+            or active_view.settings().get('git_savvy.diff_view.in_cached_mode') == in_cached_mode
+        ):
+            active_view.close()
+            return
+
         this_id = (
             repo_path,
             file_path,
@@ -151,13 +163,6 @@ class gs_diff(WindowCommand, GitCommand):
         for view in self.window.views():
             if compute_identifier_for_view(view) == this_id:
                 settings = view.settings()
-                if view == active_view and (
-                    in_cached_mode is None
-                    or settings.get("git_savvy.diff_view.in_cached_mode") == in_cached_mode
-                ):
-                    view.close()
-                    return
-
                 if in_cached_mode is not None:
                     settings.set("git_savvy.diff_view.in_cached_mode", in_cached_mode)
                 focus_view(view)

commit 3d9741a74b8a21b2edf30f1303021011d52a012f

Fix: opening multiple diff views was not possible
In the status view, a user could select multiple files with the wish
to open multiple diff views in one go.  This was broken since the
refactoring in e3519a0a (Move closing-active-view logic up, 2.41.0).

Fix it in-place instead of reverting because the code seems to be in
the right position here.

core/commands/diff.py
@@ -161,7 +161,14 @@ class gs_diff(WindowCommand, GitCommand):
         active_view = self.window.active_view()
         assert active_view
 
-        if is_diff_view(active_view) and (
+        this_id = (
+            repo_path,
+            file_path,
+            base_commit,
+            target_commit
+        )
+
+        if compute_identifier_for_view(active_view) == this_id and (
             in_cached_mode is None
             or active_view.settings().get('git_savvy.diff_view.in_cached_mode') == in_cached_mode
         ):
@@ -180,12 +187,6 @@ class gs_diff(WindowCommand, GitCommand):
                 else:
                     cur_pos = _cur_pos, rel_file_path
 
-        this_id = (
-            repo_path,
-            file_path,
-            base_commit,
-            target_commit
-        )
         for view in self.window.views():
             if compute_identifier_for_view(view) == this_id:
                 diff_view = view