bfe35431382dfa09fc5ad43e51779dd9090b023d
herr kaste <herr.kaste@gmail.com>
2024-06-20 13:13:31 +0200

    Add "github: create repo"
    Note that this initial version only supports public, user repos.  That
    is the minimum valuable enhancement.

--- github/commands/__init__.py
+++ github/commands/__init__.py
@@ -2,6 +2,7 @@ from .open_on_remote import *
 from .commit import *
 from .configure import *
 from .create_fork import *
+from .create_repo import *
 from .add_fork_as_remote import *
 from .pull_request import *
 from .open_issue import *
