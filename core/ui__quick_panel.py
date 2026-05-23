timbrel/GitSavvy

commit f159066f6c4a56b9c678712641606fa066c2c09d

Use our `show_panel`

core/ui__quick_panel.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import Callable, Iterable, NamedTuple, Sequence, Tuple, Union
+from typing import Callable, Iterable, List, NamedTuple, Sequence, Tuple, Union
 
 import sublime
 
@@ -24,7 +24,7 @@ class Action(NamedTuple):
 
 
 ActionType = Tuple[str, Callable[[], None]]
-QuickPanelItems = Iterable[Union[str, sublime.QuickPanelItem]]
+QuickPanelItems = Iterable[Union[str, List[str], sublime.QuickPanelItem]]
 
 
 def show_panel(

core/commands/flow.py
@@ -1,7 +1,5 @@
-import sublime
-
 from ..ui_mixins.input_panel import show_single_line_input_panel
-from ..ui__quick_panel import show_noop_panel
+from ..ui__quick_panel import show_noop_panel, show_panel
 from GitSavvy.core.base_commands import GsWindowCommand
 from GitSavvy.core.runtime import on_new_thread
 
@@ -52,10 +50,10 @@ class FlowMixin(GsWindowCommand):
         if not options:
             show_noop_panel(self.window, no_opts)
         else:
-            self.window.show_quick_panel(
+            show_panel(
+                self.window,
                 [help_text] + options,
-                callback,
-                flags=sublime.MONOSPACE_FONT
+                callback
             )
 
     def get_value(self, options, index):
@@ -63,8 +61,8 @@ class FlowMixin(GsWindowCommand):
         Convert a selected quick_panel index to selected option.
         Ignores first option (which is the query).
         """
-        # If the user pressed `esc` or otherwise cancelled.
-        if index == -1 or index == 0:
+        # If the user selected the help text.
+        if index == 0:
             return None
         selected = options[index - 1]  # skipping help query
         if selected.startswith('* '):

core/commands/ignore.py
@@ -6,7 +6,7 @@ from sublime_plugin import WindowCommand
 from ..git_command import GitCommand
 from ...common import util
 from ..ui_mixins.input_panel import show_single_line_input_panel
-from ..ui__quick_panel import show_noop_panel
+from ..ui__quick_panel import show_noop_panel, show_panel
 
 
 IGNORE_PATTERN_PROMPT = "Enter pattern to ignore:"
@@ -59,15 +59,9 @@ class GsAssumeUnchangedCommand(WindowCommand, GitCommand):
             f.path for f in self.get_working_dir_status().unstaged_files
         ]
 
-        self.window.show_quick_panel(
-            self._unstaged_files,
-            self.on_selection,
-            flags=sublime.MONOSPACE_FONT
-        )
+        show_panel(self.window, self._unstaged_files, self.on_selection)
 
     def on_selection(self, index):
-        if index == -1:
-            return
         fpath = self._unstaged_files[index]
         self.git("update-index", "--assume-unchanged", fpath)
 
@@ -96,16 +90,9 @@ class GsRestoreAssumedUnchangedCommand(WindowCommand, GitCommand):
         if not self._ignored_files:
             show_noop_panel(self.window, "No files are assumed unchanged.")
         else:
-            self.window.show_quick_panel(
-                self._ignored_files,
-                self.on_selection,
-                flags=sublime.MONOSPACE_FONT
-            )
+            show_panel(self.window, self._ignored_files, self.on_selection)
 
     def on_selection(self, index):
-        if index == -1:
-            return
-
         fpath = self._ignored_files[index]
         self.git("update-index", "--no-assume-unchanged", fpath)
 

core/commands/log.py
@@ -9,6 +9,7 @@ import sublime
 from ..git_command import GitCommand
 from ..ui_mixins.quick_panel import PanelActionMixin, PanelCommandMixin
 from ..ui_mixins.quick_panel import show_log_panel, show_branch_panel
+from ..ui__quick_panel import show_panel
 from ..view import capture_cur_position, Position
 from ...common import util
 from GitSavvy.core.fns import chain
@@ -150,16 +151,14 @@ class gs_log_by_author(LogMixin, WindowCommand, GitCommand):
         except ValueError:
             selected_index = 0
 
-        self.window.show_quick_panel(
+        show_panel(
+            self.window,
             [entry[3] for entry in self._entries],
             lambda index: self.on_author_selection(index, **kwargs),
-            flags=sublime.MONOSPACE_FONT,
             selected_index=selected_index
         )
 
     def on_author_selection(self, index, **kwargs):
-        if index == -1:
-            return
         self._selected_author = self._entries[index][3]
         super().run_async(**kwargs)
 

core/commands/log_graph.py
@@ -46,7 +46,7 @@ from ..view import (
 )
 from ..ui_mixins.input_panel import show_single_line_input_panel
 from ..ui__busy_spinner import start_busy_indicator, stop_busy_indicator
-from ..ui__quick_panel import SEPARATOR
+from ..ui__quick_panel import SEPARATOR, show_panel
 from ..ui__toast_popup import show_toast
 from ..ui_mixins.quick_panel import show_branch_panel
 from ..utils import add_selection_to_jump_history, flash, focus_view, Cache
@@ -1446,8 +1446,6 @@ class gs_log_graph_by_author(WindowCommand, GitCommand):
             entries.append((commit_count, author_name, author_email, author_text))
 
         def on_select(index):
-            if index == -1:
-                return
             selected_author = entries[index][3]
             self.window.run_command('gs_graph', {
                 'file_path': file_path,
@@ -1455,10 +1453,10 @@ class gs_log_graph_by_author(WindowCommand, GitCommand):
             })
 
         email = self.git("config", "user.email").strip()
-        self.window.show_quick_panel(
+        show_panel(
+            self.window,
             [entry[3] for entry in entries],
             on_select,
-            flags=sublime.MONOSPACE_FONT,
             selected_index=[line[2] for line in entries].index(email)
         )
 
@@ -1891,9 +1889,6 @@ class gs_log_graph_edit_files(TextCommand, GitCommand):
         )
 
         def on_done(idx, event={}):
-            if idx < 0:
-                return
-
             selected = items[idx]
             unselect = selected[0] == ">"
             path = selected[3:]
@@ -1912,7 +1907,8 @@ class gs_log_graph_edit_files(TextCommand, GitCommand):
                     "selected_index": max(0, idx - 1) if unselect else next_paths.index(path)
                 })
 
-        window.show_quick_panel(
+        show_panel(
+            window,
             items,
             on_done,
             flags=sublime.MONOSPACE_FONT | (
@@ -2759,9 +2755,6 @@ class gs_log_graph_action(WindowCommand, GitCommand):
             return
 
         def on_action_selection(index):
-            if index == -1:
-                return
-
             self.selected_index = index
             description, action = actions[index]
             action()
@@ -2772,10 +2765,10 @@ class gs_log_graph_action(WindowCommand, GitCommand):
             if selected_action == SEPARATOR:
                 selected_index += 1
 
-        self.window.show_quick_panel(
+        show_panel(
+            self.window,
             [a[0] for a in actions],
             on_action_selection,
-            flags=sublime.MONOSPACE_FONT,
             selected_index=selected_index,
         )
 

core/commands/log_graph_rebase_actions.py
@@ -16,7 +16,7 @@ from GitSavvy.core.git_command import GitCommand, GitSavvyError
 from GitSavvy.core.parse_diff import TextRange
 from GitSavvy.core.runtime import on_new_thread, run_on_new_thread, throttled
 from GitSavvy.core.ui_mixins.input_panel import show_single_line_input_panel
-from GitSavvy.core.ui__quick_panel import noop, show_actions_panel, SEPARATOR
+from GitSavvy.core.ui__quick_panel import noop, show_actions_panel, SEPARATOR, show_panel
 from GitSavvy.core.utils import flash, yes_no_switch
 from GitSavvy.core.view import replace_view_content
 from . import multi_selector
@@ -445,9 +445,6 @@ class gs_rebase_action(GsWindowCommand):
             actions = multi_commits_actions()
 
         def on_action_selection(index):
-            if index == -1:
-                return
-
             self.selected_index = index
             description, action = actions[index]
             action()
@@ -458,10 +455,10 @@ class gs_rebase_action(GsWindowCommand):
             if selected_action == SEPARATOR:
                 selected_index += 1
 
-        self.window.show_quick_panel(
+        show_panel(
+            self.window,
             [a[0] for a in actions],
             on_action_selection,
-            flags=sublime.MONOSPACE_FONT,
             selected_index=selected_index,
         )
 

core/commands/quick_stage.py
@@ -4,6 +4,7 @@ from sublime_plugin import WindowCommand
 
 from ..git_command import GitCommand
 from ...common import util
+from ..ui__quick_panel import show_panel
 
 
 MenuOption = namedtuple("MenuOption", ["requires_action", "menu_text", "filename", "is_untracked"])
@@ -44,9 +45,6 @@ class GsQuickStageCommand(WindowCommand, GitCommand):
         menu_entries = [f.menu_text for f in menu_options]
 
         def on_selection(id):
-            if id == -1:
-                return
-
             selection = menu_options[id]
 
             if not selection.requires_action:
@@ -86,11 +84,7 @@ class GsQuickStageCommand(WindowCommand, GitCommand):
 
             sublime.set_timeout_async(self.run_async, 0)
 
-        self.window.show_quick_panel(
-            menu_entries,
-            on_selection,
-            flags=sublime.MONOSPACE_FONT
-        )
+        show_panel(self.window, menu_entries, on_selection)
 
     def get_menu_options(self):
         """

core/commands/reset.py
@@ -1,8 +1,8 @@
-import sublime
 from .log import LogMixin
 from .reflog import RefLogMixin
 from ...common import util
 from ..ui_mixins.quick_panel import show_branch_panel
+from ..ui__quick_panel import show_panel
 from GitSavvy.core.base_commands import GsWindowCommand
 
 
@@ -52,14 +52,12 @@ class ResetMixin(GsWindowCommand):
             selected_index = -1
 
         def on_done(index):
-            if index == -1:
-                return
             self.on_reset(reset_modes[index][0].strip())
 
-        self.window.show_quick_panel(
+        show_panel(
+            self.window,
             reset_modes,
             on_done,
-            flags=sublime.MONOSPACE_FONT,
             selected_index=selected_index
         )
 

commit f70e1f9fd92c2308819df96977999cff69188be6

Automatically handle quick panel WANT_EVENT
Accept Sublime's optional quick panel event argument in the
internal callback wrapper.  Forward it to GitSavvy callbacks only
when the caller requested WANT_EVENT.

This unbreaks `gs_log_graph_edit_files`. Bug was introduced by f159066f
(Use our `show_panel`), released in 2.53.0, Dec 16 2025.

core/ui__quick_panel.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import Callable, Iterable, List, NamedTuple, Sequence, Tuple, Union
+from typing import Callable, Iterable, List, NamedTuple, Optional, Sequence, Tuple, Union
 
 import sublime
 
@@ -30,15 +30,19 @@ QuickPanelItems = Iterable[Union[str, List[str], sublime.QuickPanelItem]]
 def show_quick_panel(
     window: sublime.Window,
     items: QuickPanelItems,
-    on_done: Callable[[int], None],
+    on_done: Callable[..., None],
     on_cancel: Callable[[], None] = lambda: None,
     on_highlight: Callable[[int], None] = lambda _: None,
     selected_index: int = -1,
     flags: int = sublime.MONOSPACE_FONT,
 ) -> None:
-    def _on_done(idx: int) -> None:
+    wants_event = bool(flags & getattr(sublime, "WANT_EVENT", 0))
+
+    def _on_done(idx: int, event: Optional[dict] = None) -> None:
         if idx == -1:
             on_cancel()
+        elif wants_event:
+            on_done(idx, event)
         else:
             on_done(idx)
 